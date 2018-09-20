1ca23bfc42194805015ea1cb94905eaf2887e626
Stephen Celis <stephen@stephencelis.com>
2018-09-20 12:55:54 -0400

    add >>>

---
+++ 0031-arbitrary/Arbitrary.playground/Sources/Utils.swift
@@ -0,0 +1,7 @@
+precedencegroup ForwardCompose {
+  associativity: left
+}
+infix operator >>>: ForwardCompose
+public func >>> <A, B, C>(f: @escaping (A) -> B, g: @escaping (B) -> C) -> (A) -> C {
+  return { g(f($0)) }
+}
