1eae2108890c8b31e027907d9da4d28135a4eea7
Stephen Celis <stephen@stephencelis.com>
2018-10-23 14:51:46 -0400

    simplify more

--- xyzw-snapshot-testing/SnapshotTestingTests/SnapshotTestingTests.swift
+++ xyzw-snapshot-testing/SnapshotTestingTests/SnapshotTestingTests.swift
@@ -15,36 +15,32 @@ class ViewTests: SnapshotTestCase {
 }
 
 class SnapshotTestCase: XCTestCase {
+
   var record = false
 
   func assertSnapshot(
     matching view: UIView,
     file: StaticString = #file,
     function: String = #function,
-    line: UInt = #line
-    ) {
+    line: UInt = #line) {
 
     let snapshot = UIGraphicsImageRenderer(size: view.bounds.size)
       .image { ctx in view.layer.render(in: ctx.cgContext) }
-
     let referenceUrl = snapshotUrl(file: file, function: function)
       .appendingPathExtension("png")
-    guard
-      !self.record,
-      let referenceData = try? Data(contentsOf: referenceUrl)
-      else {
-        try! snapshot.pngData()!.write(to: referenceUrl)
-        XCTFail("Recorded: …\n\"\(referenceUrl.path)\"", file: file, line: line)
-        return
-    }
 
-    let reference = UIImage(data: referenceData)!
-    if let difference = Diff.images(reference, snapshot) {
-      XCTFail("Snapshot mismatch", file: file, line: line)
+    if !self.record, let referenceData = try? Data(contentsOf: referenceUrl) {
+      let reference = UIImage(data: referenceData)!
+      guard let difference = Diff.images(reference, snapshot) else { return }
+      XCTFail("Snapshot doesn't match reference", file: file, line: line)
       XCTContext.runActivity(named: "Attached failure diff") { activity in
         [reference, snapshot, difference]
           .forEach { image in activity.add(XCTAttachment(image: image)) }
       }
     }
+
+    try! snapshot.pngData()!.write(to: referenceUrl)
+    XCTFail("Recorded: …\n\"\(referenceUrl.path)\"", file: file, line: line)
   }
+
 }
