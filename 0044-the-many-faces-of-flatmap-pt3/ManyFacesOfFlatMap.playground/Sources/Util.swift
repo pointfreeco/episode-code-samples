94662068ee7e9ea37b0f680fbbd14860091a3528
Brandon Williams <mbw234@gmail.com>
2019-01-18 16:59:28 -0500

    wip

--- 0044-the-many-faces-of-flatmap-pt3/ManyFacesOfFlatMap.playground/Sources/Util.swift
+++ 0044-the-many-faces-of-flatmap-pt3/ManyFacesOfFlatMap.playground/Sources/Util.swift
@@ -1,4 +1,16 @@
-infix operator >>>
+precedencegroup EffectfulComposition {
+  associativity: left
+  higherThan: AssignmentPrecedence
+}
+
+infix operator >=>: EffectfulComposition
+
+precedencegroup ForwardComposition {
+  associativity: left
+  higherThan: EffectfulComposition
+}
+
+infix operator >>>: ForwardComposition
 
 public func >>> <A, B, C>(f: @escaping (A) -> B, g: @escaping (B) -> C) -> (A) -> C {
   return { a in g(f(a)) }
