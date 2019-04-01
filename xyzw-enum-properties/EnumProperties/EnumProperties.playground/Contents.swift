4053935e5ffcb681d69d06de5a685c7d8ff7cce7
Brandon Williams <mbw234@gmail.com>
2019-04-01 10:27:06 -0400

    updated code

--- xyzw-enum-properties/EnumProperties/EnumProperties.playground/Contents.swift
+++ xyzw-enum-properties/EnumProperties/EnumProperties.playground/Contents.swift
@@ -17,13 +17,20 @@ let tree = try SyntaxTreeParser.parse(
 // }
 
 class Visitor: SyntaxVisitor {
-  override func visit(_ node: EnumDeclSyntax) {
+  override func visitPre(_ node: Syntax) {
+
+  }
+  override func visitPost(_ node: Syntax) {
+    if node is EnumDeclSyntax {
+      print("}")
+    }
+  }
+  override func visit(_ node: EnumDeclSyntax) -> SyntaxVisitorContinueKind {
     print("extension \(node.identifier.withoutTrivia()) {")
-    super.visit(node)
-    print("}")
+    return .visitChildren
   }
 
-  override func visit(_ node: EnumCaseDeclSyntax) {
+  override func visit(_ node: EnumCaseDeclSyntax) -> SyntaxVisitorContinueKind {
     node.elements.forEach {
 
       let propertyType: String
@@ -50,11 +57,13 @@ class Visitor: SyntaxVisitor {
       print("    return self.\($0.identifier) != nil")
       print("  }")
     }
+    return .skipChildren
   }
 }
 
 let visitor = Visitor()
-visitor.visit(tree)
+//visitor.visit(tree)
+tree.walk(visitor)
 
 enum Validated<Valid, Invalid> {
   case valid(Valid)
@@ -71,7 +80,6 @@ enum Loading<A> {
   case loaded(A)
   case cancelled
 }
-
 extension Validated {
   var valid: Valid? {
     guard case let .valid(value) = self else { return nil }
@@ -128,31 +136,94 @@ extension Loading {
   }
 }
 
-let link = Node.element(tag: "a", attributes: ["href": "/"], children: [.text(content: "There's no place like home")])
 
-link.element?.tag
+//
+//extension Validated {
+//  var valid: Valid? {
+//    guard case let .valid(value) = self else { return nil }
+//    return value
+//  }
+//  var isValid: Bool {
+//    return self.valid != nil
+//  }
+//  var invalid: [Invalid]? {
+//    guard case let .invalid(value) = self else { return nil }
+//    return value
+//  }
+//  var isInvalid: Bool {
+//    return self.invalid != nil
+//  }
+//}
+//extension Node {
+//  var element: (tag: String, attributes: [String: String], children: [Node])? {
+//    guard case let .element(value) = self else { return nil }
+//    return value
+//  }
+//  var isElement: Bool {
+//    return self.element != nil
+//  }
+//  var text: String? {
+//    guard case let .text(value) = self else { return nil }
+//    return value
+//  }
+//  var isText: Bool {
+//    return self.text != nil
+//  }
+//}
+//extension Loading {
+//  var loading: Void? {
+//    guard case .loading = self else { return nil }
+//    return ()
+//  }
+//  var isLoading: Bool {
+//    return self.loading != nil
+//  }
+//  var loaded: A? {
+//    guard case let .loaded(value) = self else { return nil }
+//    return value
+//  }
+//  var isLoaded: Bool {
+//    return self.loaded != nil
+//  }
+//  var cancelled: Void? {
+//    guard case .cancelled = self else { return nil }
+//    return ()
+//  }
+//  var isCancelled: Bool {
+//    return self.cancelled != nil
+//  }
+//}
+//
+//let link = Node.element(tag: "a", attributes: ["href": "/"], children: [.text(content: "There's no place like home")])
+//
+//link.element?.tag
+//
+//link.element?.children
+//  .compactMap { $0.text }
+//
+//link.text
+//
+//let status = Loading<String>.loading
+////status == .loading
+//status.loading != nil
+//status.isLoading
+//
+//let requests: [Loading<String>] = [
+//  .cancelled,
+//  .loaded("Blob's Travel Blog"),
+//  .loading,
+//  .loading,
+//  .loaded("Blob's Food Blog"),
+//  .loading,
+//]
+//
+//requests.compactMap { $0.loaded }
+//
+//requests.filter { $0.isLoading }.count
+//
+//requests.filter { $0.isCancelled }.count
 
-link.element?.children
-  .compactMap { $0.text }
 
-link.text
+1
 
-let status = Loading<String>.loading
-//status == .loading
-status.loading != nil
-status.isLoading
 
-let requests: [Loading<String>] = [
-  .cancelled,
-  .loaded("Blob's Travel Blog"),
-  .loading,
-  .loading,
-  .loaded("Blob's Food Blog"),
-  .loading,
-]
-
-requests.compactMap { $0.loaded }
-
-requests.filter { $0.isLoading }.count
-
-requests.filter { $0.isCancelled }.count
--- xyzw-enum-properties/EnumProperties/Package.swift
+++ xyzw-enum-properties/EnumProperties/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.40200.0")),
+        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50000.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
