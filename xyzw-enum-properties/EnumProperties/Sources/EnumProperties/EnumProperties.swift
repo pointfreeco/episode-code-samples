716c2f48d5af8c1dabacf5225b93d955009b34a7
Stephen Celis <stephen@stephencelis.com>
2019-04-02 10:18:30 -0400

    sketch

--- xyzw-enum-properties/EnumProperties/Sources/EnumProperties/EnumProperties.swift
+++ xyzw-enum-properties/EnumProperties/Sources/EnumProperties/EnumProperties.swift
@@ -2,10 +2,33 @@ import SwiftSyntax
 
 public class Visitor: SyntaxVisitor {
   public private(set) var output = ""
+  private var cases: [
+    (
+    identifier: String,
+    label: String,
+    propertyType: String,
+    pattern: String,
+    returnValue: String
+    )
+    ] = []
 
   override public func visitPost(_ node: Syntax) {
     if node is EnumDeclSyntax {
+      print("  func fold<A>(", to: &self.output)
+      print(
+        cases.map { "    \($0.label): \($0.propertyType) -> A" }.joined(separator: ",\n"),
+        to: &self.output
+      )
+      print("  ) -> A {", to: &self.output)
+      print("    switch self {", to: &self.output)
+      cases.forEach {
+        print("    \($0.pattern):", to: &self.output)
+        print("      return \($0.label)\($0.returnValue)", to: &self.output)
+      }
+      print("    }", to: &self.output)
+      print("  }", to: &self.output)
       print("}", to: &self.output)
+      self.cases.removeAll()
     }
   }
 
@@ -38,6 +61,15 @@ public class Visitor: SyntaxVisitor {
     print("  var is\(capitalizedIdentifier): Bool {", to: &self.output)
     print("    return self.\(node.identifier) != nil", to: &self.output)
     print("  }", to: &self.output)
+    self.cases.append(
+      (
+        identifier,
+        "if\(capitalizedIdentifier)",
+        propertyType,
+        pattern,
+        returnValue == "()" ? returnValue : "(\(returnValue))"
+      )
+    )
     return .skipChildren
   }
 }
