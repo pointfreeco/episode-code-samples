67b36a03d69f3f8128e65fe6830b0489576f3753
Alexander Sokol <ixrevo@me.com>
2019-11-20 22:24:35 +0300

    Duplicate the project folder

---
+++ 0081-combine-and-effects-pt2/RxSignalPrimeTime/Counter/Counter.h
@@ -0,0 +1,19 @@
+//
+//  Counter.h
+//  Counter
+//
+//  Created by Stephen Celis on 9/8/19.
+//  Copyright © 2019 Point-Free. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Counter.
+FOUNDATION_EXPORT double CounterVersionNumber;
+
+//! Project version string for Counter.
+FOUNDATION_EXPORT const unsigned char CounterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Counter/PublicHeader.h>
+
+
