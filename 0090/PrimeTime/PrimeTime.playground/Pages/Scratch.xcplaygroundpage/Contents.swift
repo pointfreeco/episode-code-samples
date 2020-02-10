3006a683cb6c52b8f1eb0c42a3b3c642e135381c
Stephen Celis <stephen@stephencelis.com>
2020-02-10 13:08:17 -0500

    WIP

--- 0090/PrimeTime/PrimeTime.playground/Pages/Scratch.xcplaygroundpage/Contents.swift
+++ 0090/PrimeTime/PrimeTime.playground/Pages/Scratch.xcplaygroundpage/Contents.swift
@@ -1,24 +1,4 @@
 import ComposableArchitecture
+import PrimeModal
 
-func pullback<GlobalValue, LocalValue, GlobalAction, LocalAction>(
-  reducer: @escaping Reducer<LocalValue, LocalAction>,
-  value: WritableKeyPath<GlobalValue, LocalValue>,
-  action: WritableKeyPath<GlobalAction, LocalAction?>
-) -> Reducer<GlobalValue, GlobalAction> {
-  return { globalValue, globalAction in
-    guard let localAction = globalAction[keyPath: action] else { return [] }
-    let localEffects = reducer(&globalValue[keyPath: value], localAction)
-    return localEffects
-      .map { localEffect in
-        localEffect
-          .map { localAction in
-            var globalAction = globalAction
-            globalAction[keyPath: action] = localAction
-            return globalAction
-        }
-      .eraseToEffect()
-    }
-  }
-}
-
-
+let store = Store<Void, Never>(initialValue: (), reducer: { _, _ in [] })
