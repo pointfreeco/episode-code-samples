6e981fc7853e8fa8ea86bf5ffcb4b428a42235cc
Sven A. Schmidt <sas@finestructure.co>
2020-03-23 15:14:18 +0100

    Update to HistoryTransceiver 0.0.8, trimming setup further

--- 0090-composing-architecture-with-case-paths/PrimeTime/PrimeTime/SceneDelegate.swift
+++ 0090-composing-architecture-with-case-paths/PrimeTime/PrimeTime/SceneDelegate.swift
@@ -3,19 +3,12 @@ import HistoryTransceiver
 import SwiftUI
 import UIKit
 
-var appStore = HistoryTransceiverView<ContentView>.store()
-
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    Transceiver.shared.receive(Message.self) { msg in
-        if msg.command == .reset {
-            appStore.send(.updateState(msg.state))
-        }
-    }
-    Transceiver.shared.resume()
-    let contentView = HistoryTransceiverView<ContentView>(store: appStore)
+    let contentView = HistoryTransceiverView<ContentView>()
+    contentView.resume()
 
     if let windowScene = scene as? UIWindowScene {
       let window = UIWindow(windowScene: windowScene)
