b11771c69b8bd0b0232b7923118f685d8003c869
Point-Free <point-free@Brandons-MacBook-Pro.local>
2020-05-29 10:18:25 -0700

    clean up

--- xyzw-combine-schedulers/CombineSchedulers/CombineSchedulers/ContentView.swift
+++ xyzw-combine-schedulers/CombineSchedulers/CombineSchedulers/ContentView.swift
@@ -1,21 +1,13 @@
-//
-//  ContentView.swift
-//  CombineSchedulers
-//
-//  Created by Point-Free on 5/29/20.
-//  Copyright © 2020 Point-Free. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+  var body: some View {
+    Text("Hello, World!")
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
