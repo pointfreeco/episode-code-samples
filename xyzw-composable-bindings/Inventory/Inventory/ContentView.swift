e25678265fb7f0a6a65c336d75e7de445e9e49d3
Point-Free <pointfreeco@Stephens-MBP-16.home>
2020-06-24 16:02:10 -0400

    wip

--- xyzw-composable-bindings/Inventory/Inventory/ContentView.swift
+++ xyzw-composable-bindings/Inventory/Inventory/ContentView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
-struct Item {
+struct Item: Hashable, Identifiable {
+  let id = UUID()
   var name: String
   var color: Color?
 //  var quantity = 1
@@ -8,14 +9,14 @@ struct Item {
 //  var isOnBackOrder = false
   var status: Status
   
-  enum Status {
+  enum Status: Hashable {
     case inStock(quantity: Int)
     case outOfStock(isOnBackOrder: Bool)
     
-//    var isInStock: Bool {
-//      guard case .inStock = self else { return false }
-//      return true
-//    }
+    var isInStock: Bool {
+      guard case .inStock = self else { return false }
+      return true
+    }
     
 //    var quantity: Int? {
 //      get {
@@ -66,6 +67,23 @@ struct Item {
     case red
     case yellow
     case white
+    
+    var toSwiftUIColor: SwiftUI.Color {
+      switch self {
+      case .blue:
+        return .blue
+      case .green:
+        return .green
+      case .black:
+        return .black
+      case .red:
+        return .red
+      case .yellow:
+        return .yellow
+      case .white:
+        return .white
+      }
+    }
   }
 
 //  static func inStock(
@@ -192,9 +210,125 @@ struct ItemView: View {
   }
 }
 
+class InventoryViewModel: ObservableObject {
+  @Published var draft: Item?
+  @Published var inventory: [Item]
+  
+  init(
+    inventory: [Item] = []
+  ) {
+    self.inventory = inventory
+  }
+  
+  func addButtonTapped() {
+    self.draft = Item(name: "", color: nil, status: .inStock(quantity: 1))
+  }
+  
+  func cancelButtonTapped() {
+    self.draft = nil
+  }
+  
+  func saveButtonTapped() {
+    if let item = self.draft {
+      self.inventory.append(item)
+    }
+    self.draft = nil
+  }
+  
+  func duplicate(item: Item) {
+    self.draft = Item(name: item.name, color: item.color, status: item.status) // item.duplicate()
+  }
+}
+
+struct InventoryView: View {
+  @ObservedObject var viewModel: InventoryViewModel
+  
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(self.viewModel.inventory, id: \.self) { item in
+          HStack {
+            VStack(alignment: .leading) {
+              Text(item.name)
+
+              Group { () -> Text in
+                switch item.status {
+                case let .inStock(quantity):
+                  return Text("In stock: \(quantity)")
+                case let .outOfStock(isOnBackOrder):
+                  return Text("Out of stock" + (isOnBackOrder ? ": on back order" : ""))
+                }
+              }
+            }
+
+            Spacer()
+
+            item.color.map { color in
+              Rectangle()
+                .frame(width: 30, height: 30)
+                .foregroundColor(color.toSwiftUIColor)
+                .border(Color.black, width: 1)
+            }
+            
+            Button(action: { self.viewModel.duplicate(item: item) }) {
+              Image(systemName: "doc.on.doc.fill")
+            }
+            .padding(.leading)
+          }
+          .buttonStyle(PlainButtonStyle())
+          .foregroundColor(item.status.isInStock ? nil : Color.gray)
+        }
+      }
+      .navigationBarTitle("Inventory")
+      .navigationBarItems(
+        trailing: Button("Add") {
+          self.viewModel.addButtonTapped()
+        }
+      )
+//        .sheet(item: self.$viewModel.draft) { _ in
+//          self.$viewModel.draft.unwrap().map { item in
+        .sheet(unwrap: self.$viewModel.draft) { item in
+          NavigationView {
+            ItemView(item: item)
+              .navigationBarItems(
+                leading: Button("Cancel") { self.viewModel.cancelButtonTapped() },
+                trailing: Button("Save") { self.viewModel.saveButtonTapped() }
+            )
+          }
+      }
+    }
+  }
+}
+
+extension View {
+  func sheet<Item, Content>(
+    unwrap item: Binding<Item?>,
+    content: @escaping (Binding<Item>) -> Content
+  ) -> some View where Item: Identifiable, Content: View {
+    self.sheet(item: item) { _ in
+      item.unwrap().map(content)
+    }
+  }
+}
+
 struct ContentView: View {
   var body: some View {
     Text("Hello, World!")
+  }
+}
+
+struct InventoryView_Previews: PreviewProvider {
+  static var previews: some View {
+    InventoryView(
+      viewModel: InventoryViewModel(
+        inventory: [
+          Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100)),
+          Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20)),
+          Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true)),
+          Item(name: "Headphones", color: .green, status: .outOfStock(isOnBackOrder: false)),
+        ]
+      )
+    )
   }
 }
 
