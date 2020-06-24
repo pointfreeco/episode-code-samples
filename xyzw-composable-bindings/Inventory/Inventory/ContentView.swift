72fcc955321ac795d3cb9f8c57d9d603c1cab819
Point-Free <point-free@Brandons-MacBook-Pro.local>
2020-06-24 12:19:41 -0500

    wip

---
+++ xyzw-composable-bindings/Inventory/Inventory/ContentView.swift
@@ -0,0 +1,95 @@
+import SwiftUI
+
+struct Item {
+  var name: String
+  var color: Color?
+  var quantity = 1
+  var isInStock = true
+  var isOnBackOrder = false
+
+  enum Color: String, CaseIterable {
+    case blue
+    case green
+    case black
+    case red
+    case yellow
+    case white
+  }
+
+  static func inStock(
+    name: String,
+    color: Color?,
+    quantity: Int
+  ) -> Self {
+    Item(name: name, color: color, quantity: quantity, isInStock: true, isOnBackOrder: false)
+  }
+
+  static func outOfStock(
+    name: String,
+    color: Color?,
+    isOnBackOrder: Bool
+  ) -> Self {
+    Item(name: name, color: color, quantity: 0, isInStock: false, isOnBackOrder: isOnBackOrder)
+  }
+}
+
+struct ItemView: View {
+  @Binding var item: Item
+
+  var body: some View {
+    Form {
+      TextField("Name", text: self.$item.name)
+
+      Picker(selection: self.$item.color, label: Text("Color")) {
+        Text("None")
+          .tag(Item.Color?.none)
+
+        ForEach(Item.Color.allCases, id: \.rawValue) { color in
+          Text(color.rawValue)
+            .tag(Optional(color))
+        }
+      }
+
+      if self.item.isInStock {
+        Section(header: Text("In stock")) {
+          Stepper("Quantity: \(self.item.quantity)", value: self.$item.quantity)
+          Button("Mark as sold out") {
+            self.item.quantity = 0
+            self.item.isInStock = false
+          }
+        }
+      } else {
+        Section(header: Text("Out of stock")) {
+          Toggle("Is on back order?", isOn: self.$item.isOnBackOrder)
+          Button("Is back in stock!") {
+            self.item.quantity = 1
+            self.item.isInStock = true
+          }
+        }
+      }
+    }
+  }
+}
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+
+    struct Wrapper: View {
+      @State var item = Item(name: "Keyboard", color: .green)
+
+      var body: some View {
+        ItemView(item: self.$item)
+      }
+    }
+
+    return NavigationView {
+      Wrapper()
+    }
+  }
+}
