6523d6d2acbaca8974399aa97a465b12f37fce3e
Point-Free <pointfreeco@Stephens-MBP-16.home>
2020-06-24 13:54:06 -0400

    wip

--- xyzw-composable-bindings/Inventory/Inventory/ContentView.swift
+++ xyzw-composable-bindings/Inventory/Inventory/ContentView.swift
@@ -3,9 +3,58 @@ import SwiftUI
 struct Item {
   var name: String
   var color: Color?
-  var quantity = 1
-  var isInStock = true
-  var isOnBackOrder = false
+//  var quantity = 1
+//  var isInStock = true
+//  var isOnBackOrder = false
+  var status: Status
+  
+  enum Status {
+    case inStock(quantity: Int)
+    case outOfStock(isOnBackOrder: Bool)
+    
+    var isInStock: Bool {
+      guard case .inStock = self else { return false }
+      return true
+    }
+    
+    var quantity: Int {
+      get {
+        switch self {
+        case .inStock(quantity: let quantity):
+          return quantity
+        case .outOfStock:
+          return 0
+        }
+      }
+      set {
+//        switch self {
+//        case .inStock:
+          self = .inStock(quantity: newValue)
+//        case .outOfStock:
+//          break
+//        }
+      }
+    }
+    
+    var isOnBackOrder: Bool {
+      get {
+        guard case let .outOfStock(isOnBackOrder) = self else {
+          return false
+//          return true
+//          return nil
+        }
+        return isOnBackOrder
+      }
+      set {
+//        switch self {
+//        case .inStock:
+//          break
+//        case .outOfStock:
+          self = .outOfStock(isOnBackOrder: newValue)
+//        }
+      }
+    }
+  }
 
   enum Color: String, CaseIterable {
     case blue
@@ -16,21 +65,21 @@ struct Item {
     case white
   }
 
-  static func inStock(
-    name: String,
-    color: Color?,
-    quantity: Int
-  ) -> Self {
-    Item(name: name, color: color, quantity: quantity, isInStock: true, isOnBackOrder: false)
-  }
-
-  static func outOfStock(
-    name: String,
-    color: Color?,
-    isOnBackOrder: Bool
-  ) -> Self {
-    Item(name: name, color: color, quantity: 0, isInStock: false, isOnBackOrder: isOnBackOrder)
-  }
+//  static func inStock(
+//    name: String,
+//    color: Color?,
+//    quantity: Int
+//  ) -> Self {
+//    Item(name: name, color: color, quantity: quantity, isInStock: true, isOnBackOrder: false)
+//  }
+//
+//  static func outOfStock(
+//    name: String,
+//    color: Color?,
+//    isOnBackOrder: Bool
+//  ) -> Self {
+//    Item(name: name, color: color, quantity: 0, isInStock: false, isOnBackOrder: isOnBackOrder)
+//  }
 }
 
 struct ItemView: View {
@@ -50,20 +99,22 @@ struct ItemView: View {
         }
       }
 
-      if self.item.isInStock {
+      if self.item.status.isInStock {
         Section(header: Text("In stock")) {
-          Stepper("Quantity: \(self.item.quantity)", value: self.$item.quantity)
+          Stepper("Quantity: \(self.item.status.quantity)", value: self.$item.status.quantity)
           Button("Mark as sold out") {
-            self.item.quantity = 0
-            self.item.isInStock = false
+//            self.item.quantity = 0
+//            self.item.isInStock = false
+            self.item.status = .outOfStock(isOnBackOrder: false)
           }
         }
       } else {
         Section(header: Text("Out of stock")) {
-          Toggle("Is on back order?", isOn: self.$item.isOnBackOrder)
+          Toggle("Is on back order?", isOn: self.$item.status.isOnBackOrder)
           Button("Is back in stock!") {
-            self.item.quantity = 1
-            self.item.isInStock = true
+//            self.item.quantity = 1
+//            self.item.isInStock = true
+            self.item.status = .inStock(quantity: 1)
           }
         }
       }
@@ -81,7 +132,7 @@ struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
 
     struct Wrapper: View {
-      @State var item = Item(name: "Keyboard", color: .green)
+      @State var item = Item(name: "Keyboard", color: .green, status: .inStock(quantity: 1))
 
       var body: some View {
         ItemView(item: self.$item)
