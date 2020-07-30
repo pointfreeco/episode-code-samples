5677f9a8b3e10596becb564c8ec371fb58ca64d6
Point-Free <point-free@Brandons-MacBook-Pro.local>
2020-07-30 15:26:42 -0400

    wip

--- xyzw-parsing-recap/ParsingRecap.playground/Contents.swift
+++ xyzw-parsing-recap/ParsingRecap.playground/Contents.swift
@@ -328,3 +328,159 @@ let money = zip(currency, .double)
 money.run("$100")
 money.run("£100")
 money.run("€100")
+
+
+let upcomingRaces = """
+  New York City, $300
+  40.60248° N, 74.06433° W
+  40.61807° N, 74.02966° W
+  40.64953° N, 74.00929° W
+  40.67884° N, 73.98198° W
+  40.69894° N, 73.95701° W
+  40.72791° N, 73.95314° W
+  40.74882° N, 73.94221° W
+  40.75740° N, 73.95309° W
+  40.76149° N, 73.96142° W
+  40.77111° N, 73.95362° W
+  40.80260° N, 73.93061° W
+  40.80409° N, 73.92893° W
+  40.81432° N, 73.93292° W
+  40.80325° N, 73.94472° W
+  40.77392° N, 73.96917° W
+  40.77293° N, 73.97671° W
+  ---
+  Berlin, €100
+  13.36015° N, 52.51516° E
+  13.33999° N, 52.51381° E
+  13.32539° N, 52.51797° E
+  13.33696° N, 52.52507° E
+  13.36454° N, 52.52278° E
+  13.38152° N, 52.52295° E
+  13.40072° N, 52.52969° E
+  13.42555° N, 52.51508° E
+  13.41858° N, 52.49862° E
+  13.40929° N, 52.48882° E
+  13.37968° N, 52.49247° E
+  13.34898° N, 52.48942° E
+  13.34103° N, 52.47626° E
+  13.32851° N, 52.47122° E
+  13.30852° N, 52.46797° E
+  13.28742° N, 52.47214° E
+  13.29091° N, 52.48270° E
+  13.31084° N, 52.49275° E
+  13.32052° N, 52.50190° E
+  13.34577° N, 52.50134° E
+  13.36903° N, 52.50701° E
+  13.39155° N, 52.51046° E
+  13.37256° N, 52.51598° E
+  ---
+  London, £500
+  51.48205° N, 0.04283° E
+  51.47439° N, 0.02170° E
+  51.47618° N, 0.02199° E
+  51.49295° N, 0.05658° E
+  51.47542° N, 0.03019° E
+  51.47537° N, 0.03015° E
+  51.47435° N, 0.03733° E
+  51.47954° N, 0.04866° E
+  51.48604° N, 0.06293° E
+  51.49314° N, 0.06104° E
+  51.49248° N, 0.04740° E
+  51.48888° N, 0.03564° E
+  51.48655° N, 0.01830° E
+  51.48085° N, 0.02223° W
+  51.49210° N, 0.04510° W
+  51.49324° N, 0.04699° W
+  51.50959° N, 0.05491° W
+  51.50961° N, 0.05390° W
+  51.49950° N, 0.01356° W
+  51.50898° N, 0.02341° W
+  51.51069° N, 0.04225° W
+  51.51056° N, 0.04353° W
+  51.50946° N, 0.07810° W
+  51.51121° N, 0.09786° W
+  51.50964° N, 0.11870° W
+  51.50273° N, 0.13850° W
+  51.50095° N, 0.12411° W
+  """
+
+struct Race {
+  let location: String
+  let entranceFee: Money
+  let path: [Coordinate]
+}
+
+extension Parser where Output == Substring {
+  static func prefix(while p: @escaping (Character) -> Bool) -> Self {
+    Self { input in
+      let output = input.prefix(while: p)
+      input.removeFirst(output.count)
+      return output
+    }
+  }
+}
+
+//let locationName = Parser.prefix(while: { $0 != "," })
+
+func zip<A, B, C, D>(
+  _ a: Parser<A>,
+  _ b: Parser<B>,
+  _ c: Parser<C>,
+  _ d: Parser<D>
+) -> Parser<(A, B, C, D)> {
+  zip(a, zip(b, c, d))
+    .map { a, bcd in (a, bcd.0, bcd.1, bcd.2) }
+}
+func zip<A, B, C, D, E>(
+  _ a: Parser<A>,
+  _ b: Parser<B>,
+  _ c: Parser<C>,
+  _ d: Parser<D>,
+  _ e: Parser<E>
+) -> Parser<(A, B, C, D, E)> {
+  zip(a, zip(b, c, d, e))
+    .map { a, bcde in (a, bcde.0, bcde.1, bcde.2, bcde.3) }
+}
+
+extension Parser {
+  func zeroOrMore(
+    separatedBy separator: Parser<Void>
+  ) -> Parser<[Output]> {
+    Parser<[Output]> { input in
+      var rest = input
+      var matches: [Output] = []
+      while let match = self.run(&input) {
+        rest = input
+        matches.append(match)
+        if separator.run(&input) == nil {
+          return matches
+        }
+      }
+      input = rest
+      return matches
+    }
+  }
+}
+
+let race = zip(
+  .prefix(while: { $0 != "," }),
+  .prefix(", "),
+  money,
+  .prefix("\n"),
+  coord.zeroOrMore(separatedBy: .prefix("\n"))
+)
+.map { locationName, _, entranceFee, _, path in
+  Race(
+    location: String(locationName),
+    entranceFee: entranceFee,
+    path: path
+  )
+}
+
+let races = race.zeroOrMore(separatedBy: .prefix("\n---\n"))
+
+dump(
+races.run(upcomingRaces)
+)
+
+// coord.zeroOrMore()
