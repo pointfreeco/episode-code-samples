497fa120393423b7a192187c7899cee479bc3ef3
Point-Free <pointfreeco@Stephens-MBP-16.home>
2020-07-30 14:18:06 -0400

    wip

--- xyzw-parsing-recap/ParsingRecap.playground/Contents.swift
+++ xyzw-parsing-recap/ParsingRecap.playground/Contents.swift
@@ -28,12 +28,12 @@ URL(string: "bad^website")
 URLComponents.init(string:) as (String) -> URLComponents?
 UUID.init(uuidString:) as (String) -> UUID?
 
-import UIKit
-
-NSCoder.cgRect(for: "{{1,2},{3,4}}")
-NSCoder.cgRect(for: "")
-NSCoder.uiEdgeInsets(for: "{1,2,3,4}")
-NSCoder.uiEdgeInsets(for: "")
+//import UIKit
+//
+//NSCoder.cgRect(for: "{{1,2},{3,4}}")
+//NSCoder.cgRect(for: "")
+//NSCoder.uiEdgeInsets(for: "{1,2,3,4}")
+//NSCoder.uiEdgeInsets(for: "")
 
 DateFormatter().date(from:) as (String) -> Date?
 NumberFormatter().number(from:) as (String) -> NSNumber?
@@ -46,36 +46,39 @@ NumberFormatter().number(from:) as (String) -> NSNumber?
 struct Parser<Output> {
   let run: (inout Substring) -> Output?
 }
-
-let int = Parser<Int> { input in
-  let original = input
-
-  let sign: Int // +1, -1
-  if input.first == "-" {
-    sign = -1
-    input.removeFirst()
-  } else if input.first == "+" {
-    sign = 1
-    input.removeFirst()
-  } else {
-    sign = 1
+// Parser<Int>.int
+// .int
+extension Parser where Output == Int {
+  static let int = Self { input in
+    let original = input
+    
+    let sign: Int // +1, -1
+    if input.first == "-" {
+      sign = -1
+      input.removeFirst()
+    } else if input.first == "+" {
+      sign = 1
+      input.removeFirst()
+    } else {
+      sign = 1
+    }
+    
+    let intPrefix = input.prefix(while: \.isNumber)
+    guard let match = Int(intPrefix)
+    else {
+      input = original
+      return nil
+    }
+    input.removeFirst(intPrefix.count)
+    return match * sign
   }
-
-  let intPrefix = input.prefix(while: \.isNumber)
-  guard let match = Int(intPrefix)
-  else {
-    input = original
-    return nil
-  }
-  input.removeFirst(intPrefix.count)
-  return match * sign
 }
 
 var input = "123 Hello"[...]
 //var input: Substring = "123 Hello"
 //var input = "123 Hello" as Substring
 
-int.run(&input)
+Parser.int.run(&input)
 input
 
 extension Parser {
@@ -86,8 +89,73 @@ extension Parser {
   }
 }
 
-int.run("123 Hello")
-int.run("+123 Hello")
-int.run("Hello Blob")
-int.run("-123 Hello")
-int.run("-Hello")
+Parser.int.run("123 Hello")
+Parser.int.run("+123 Hello")
+Parser.int.run("Hello Blob")
+Parser.int.run("-123 Hello")
+Parser.int.run("-Hello")
+
+
+extension Parser where Output == Double {
+  static let double = Self { input in
+    let original = input
+    let sign: Double
+    if input.first == "-" {
+      sign = -1
+      input.removeFirst()
+    } else if input.first == "+" {
+      sign = 1
+      input.removeFirst()
+    } else {
+      sign = 1
+    }
+  
+    var decimalCount = 0
+    let prefix = input.prefix { char in
+      if char == "." { decimalCount += 1 }
+      return char.isNumber || (char == "." && decimalCount <= 1)
+    }
+  
+    guard let match = Double(prefix)
+    else {
+      input = original
+      return nil
+    }
+  
+    input.removeFirst(prefix.count)
+  
+    return match * sign
+  }
+}
+Parser.double.run("42 hello")
+Parser.double.run("4.2 hello")
+Parser.double.run("42. hello")
+Parser.double.run(".42 hello")
+
+Parser.double.run("-42 hello")
+Parser.double.run("+42 hello")
+Parser.double.run("1.2.3 hello")
+
+
+extension Parser where Output == Character {
+  static let char = Self { input in
+    guard !input.isEmpty else { return nil }
+    return input.removeFirst()
+  }
+}
+
+Parser.char.run("Hello Blob")
+Parser.char.run("")
+
+
+extension Parser where Output == Void {
+  static func prefix(_ p: String) -> Self {
+    Self { input in
+      guard input.hasPrefix(p) else { return nil }
+      input.removeFirst(p.count)
+      return ()
+    }
+  }
+}
+
+Parser.prefix("Hello").run("Hello Blob")
