b910211643717645f4b51caacb6d046ea472ef33
Point-Free <pointfreeco@Stephens-MBP.home>
2021-01-12 12:43:50 -0500

    wip

--- xyzw-concise-forms/ConciseForms/ConciseForms/VanillaSwiftUIFormView.swift
+++ xyzw-concise-forms/ConciseForms/ConciseForms/VanillaSwiftUIFormView.swift
@@ -1,30 +1,52 @@
 import SwiftUI
 
+class SettingsViewModel: ObservableObject {
+  @Published var digest = Digest.off
+  @Published var displayName = ""
+  @Published var protectMyPosts = false
+  @Published var sendNotifications = false
+  
+  func reset() {
+    self.digest = .off
+    self.displayName = ""
+    self.protectMyPosts = false
+    self.sendNotifications = false
+  }
+}
+
 struct VanillaSwiftUIFormView: View {
+  @ObservedObject var viewModel: SettingsViewModel
+  
   var body: some View {
     Form {
       Section(header: Text("Profile")) {
-        TextField("Display name", text: .constant(""))
-        Toggle("Protect my posts", isOn: .constant(false))
+        TextField("Display name", text: self.$viewModel.displayName)
+        Toggle("Protect my posts", isOn: self.$viewModel.protectMyPosts)
       }
       Section(header: Text("Communication")) {
-        Toggle("Send notifications", isOn: .constant(false))
+        Toggle("Send notifications", isOn: self.$viewModel.sendNotifications)
 
-        Picker(
-          "Top posts digest",
-          selection: .constant(Digest.off)
-        ) {
-          Text("daily")
-          Text("weekly")
-          Text("off")
+        if self.viewModel.sendNotifications {
+          Picker(
+            "Top posts digest",
+            selection: self.$viewModel.digest
+          ) {
+            ForEach(Digest.allCases, id: \.self) { digest in
+              Text(digest.rawValue)
+            }
+          }
         }
+      }
+      
+      Button("Reset") {
+        self.viewModel.reset()
       }
     }
     .navigationTitle("Settings")
   }
 }
 
-enum Digest {
+enum Digest: String, CaseIterable {
   case daily
   case weekly
   case off
@@ -33,7 +55,9 @@ enum Digest {
 struct VanillaSwiftUIFormView_Previews: PreviewProvider {
   static var previews: some View {
     NavigationView {
-      VanillaSwiftUIFormView()
+      VanillaSwiftUIFormView(
+        viewModel: SettingsViewModel()
+      )
     }
   }
 }
