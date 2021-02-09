296ae307c1416cad1a1e06dc75b5a1dd6c026411
Point-Free <pointfreeco@Stephens-MBP.home>
2021-02-09 13:34:50 -0500

    wip

--- xyzw-animations/Animations/Animations/ContentView.swift
+++ xyzw-animations/Animations/Animations/ContentView.swift
@@ -4,20 +4,31 @@ struct ContentView: View {
   @State var circleCenter = CGPoint.zero
   @State var circleColor = Color.black
   @State var isCircleScaled = false
+  @State var isResetting = false
 
   var body: some View {
     VStack {
       Circle()
         .fill(self.circleColor)
+        .animation(.linear)
+        .overlay(Text("Hi"))
         .frame(width: 50, height: 50)
         .scaleEffect(self.isCircleScaled ? 2 : 1)
+//        .animation(nil, value: self.isCircleScaled)
+//        .animation(.disabled)
         .offset(x: self.circleCenter.x - 25, y: self.circleCenter.y - 25)
-        .animation(.spring(response: 0.3, dampingFraction: 0.1))
+        .animation(self.isResetting ? nil : .spring(response: 0.3, dampingFraction: 0.1))
         .gesture(
           DragGesture(minimumDistance: 0).onChanged { value in
             self.circleCenter = value.location
           }
         )
+        .foregroundColor(self.isCircleScaled ? .red : nil)
+//      if self.isCircleScaled {
+//        circle.foregroundColor(.red)
+//      } else {
+//        circle
+//      }
 
       Toggle("Scale", isOn: self.$isCircleScaled)
 
@@ -41,6 +52,16 @@ struct ContentView: View {
 //        DispatchQueue.main.asyncAfter(deadline:   .now() + 4) {
 //          self.circleColor = .black
 //        }
+      }
+      
+      Button("Reset") {
+        self.isResetting = true
+        self.circleCenter = .zero
+        self.circleColor = .black
+        self.isCircleScaled = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1)) {
+          self.isResetting = false
+        }
       }
     }
   }
