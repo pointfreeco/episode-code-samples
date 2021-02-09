018d40ef7f23aeb7bc6f1181b120c35d3936939c
Point-Free <point-free@Brandons-MBP-2.lan>
2021-02-09 10:58:19 -0800

    wip

--- xyzw-animations/Animations/Animations/ContentView.swift
+++ xyzw-animations/Animations/Animations/ContentView.swift
@@ -10,17 +10,19 @@ struct ContentView: View {
     VStack {
       Circle()
         .fill(self.circleColor)
-        .animation(.linear)
+//        .animation(.linear)
         .overlay(Text("Hi"))
         .frame(width: 50, height: 50)
         .scaleEffect(self.isCircleScaled ? 2 : 1)
 //        .animation(nil, value: self.isCircleScaled)
 //        .animation(.disabled)
         .offset(x: self.circleCenter.x - 25, y: self.circleCenter.y - 25)
-        .animation(self.isResetting ? nil : .spring(response: 0.3, dampingFraction: 0.1))
+//        .animation(self.isResetting ? nil : .spring(response: 0.3, dampingFraction: 0.1))
         .gesture(
           DragGesture(minimumDistance: 0).onChanged { value in
-            self.circleCenter = value.location
+            withAnimation(.spring(response: 0.3, dampingFraction: 0.1)) {
+              self.circleCenter = value.location
+            }
           }
         )
         .foregroundColor(self.isCircleScaled ? .red : nil)
@@ -30,14 +32,33 @@ struct ContentView: View {
 //        circle
 //      }
 
-      Toggle("Scale", isOn: self.$isCircleScaled)
+      Toggle(
+        "Scale",
+        isOn: self.$isCircleScaled.animation(.spring(response: 0.3, dampingFraction: 0.1))
+
+//          Binding(
+//          get: { self.isCircleScaled },
+//          set: { isOn in
+//            withAnimation(.spring(response: 0.3, dampingFraction: 0.1)) {
+//              self.isCircleScaled = isOn
+//            }
+//          }
+//        )
+
+      )
 
       Button("Cycle colors") {
         [Color.red, .blue, .green, .purple, .black]
           .enumerated()
           .forEach { offset, color in
             DispatchQueue.main.asyncAfter(deadline:   .now() + .seconds(offset)) {
-              self.circleColor = color
+              withAnimation(.linear) {
+                self.circleColor = color
+              }
+
+              // withAnimation: (() -> R) -> R
+              // Async<A> = ((A) -> Void) -> Void
+              //            ((A) -> R) -> R
             }
           }
 //        DispatchQueue.main.asyncAfter(deadline:   .now() + 1) {
@@ -55,13 +76,15 @@ struct ContentView: View {
       }
       
       Button("Reset") {
-        self.isResetting = true
-        self.circleCenter = .zero
-        self.circleColor = .black
-        self.isCircleScaled = false
-        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1)) {
-          self.isResetting = false
+//        self.isResetting = true
+        withAnimation {
+          self.circleCenter = .zero
+          self.circleColor = .black
         }
+        self.isCircleScaled = false
+//        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1)) {
+//          self.isResetting = false
+//        }
       }
     }
   }
