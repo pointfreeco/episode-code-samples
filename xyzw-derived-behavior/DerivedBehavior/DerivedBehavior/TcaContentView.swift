adae578e8624ed14c8bbf3a70dfc0b82dc5ebbdd
Point-Free <point-free@Brandons-MacBook-Pro-2.local>
2021-05-12 12:33:19 -0500

    wip

--- xyzw-derived-behavior/DerivedBehavior/DerivedBehavior/TcaContentView.swift
+++ xyzw-derived-behavior/DerivedBehavior/DerivedBehavior/TcaContentView.swift
@@ -4,19 +4,105 @@ import SwiftUI
 struct AppState: Equatable {
   var count = 0
   var favorites: Set<Int> = []
+
+  var counter: CounterState {
+    get {
+      .init(count: self.count, favorites: self.favorites)
+    }
+    set {
+      self.count = newValue.count
+      self.favorites = newValue.favorites
+    }
+  }
+  var profile: ProfileState {
+    get {
+      .init(favorites: self.favorites)
+    }
+    set {
+      self.favorites = newValue.favorites
+    }
+  }
 }
 
 enum AppAction {
-  case incrementButtonTapped
-  case decrementButtonTapped
-  case saveButtonTapped
-  case removeButtonTapped
-  case profileRemoveButtonTapped(Int)
+  case counter(CounterAction)
+  case profile(ProfileAction)
+
+//  case incrementButtonTapped
+//  case decrementButtonTapped
+//  case saveButtonTapped
+//  case removeButtonTapped
+//  case profileRemoveButtonTapped(Int)
 }
 
 struct AppEnvironment {}
 
-let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, _ in
+let appReducer = Reducer.combine(
+  counterReducer.pullback(
+    state: \AppState.counter,
+    action: /AppAction.counter,
+    environment: { (_: AppEnvironment) in CounterEnvironment() }
+  ),
+  profileReducer.pullback(
+    state: \AppState.profile,
+    action: /AppAction.profile,
+    environment: { (_: AppEnvironment) in ProfileEnvironment() }
+  )
+)
+
+//  Reducer<AppState, AppAction, AppEnvironment> { state, action, _ in
+//  switch action {
+//  case let .counter(counterAction):
+//    return counterReducer.run(&state.counter, counterAction, CounterEnvironment())
+//      .map(AppAction.counter)
+//
+//  case let .profile(profileAction):
+//    return profileReducer.run(&state.profile, profileAction, ProfileEnvironment())
+//      .map(AppAction.profile)
+//  }
+//}
+
+struct TcaContentView: View {
+  let store: Store<AppState, AppAction>
+  
+  var body: some View {
+    WithViewStore(self.store) { viewStore in
+      TabView {
+        TcaCounterView(
+          store: self.store
+            .scope(
+              state: \.counter,
+              action: AppAction.counter
+            )
+        )
+          .tabItem { Text("Counter \(viewStore.count)") }
+
+        TcaProfileView(
+          store: self.store
+            .scope(
+              state: \.profile,
+              action: AppAction.profile
+            )
+        )
+          .tabItem { Text("Profile \(viewStore.favorites.count)") }
+      }
+    }
+  }
+}
+
+struct CounterState: Equatable {
+  var count = 0
+  var favorites: Set<Int> = []
+}
+enum CounterAction {
+  case incrementButtonTapped
+  case decrementButtonTapped
+  case saveButtonTapped
+  case removeButtonTapped
+}
+struct CounterEnvironment {}
+
+let counterReducer = Reducer<CounterState, CounterAction, CounterEnvironment> { state, action, _ in
   switch action {
   case .incrementButtonTapped:
     state.count += 1
@@ -30,30 +116,11 @@ let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, _
   case .removeButtonTapped:
     state.favorites.remove(state.count)
     return .none
-  case let .profileRemoveButtonTapped(number):
-    state.favorites.remove(number)
-    return .none
-  }
-}
-
-struct TcaContentView: View {
-  let store: Store<AppState, AppAction>
-  
-  var body: some View {
-    WithViewStore(self.store) { viewStore in
-      TabView {
-        TcaCounterView(store: self.store)
-          .tabItem { Text("Counter \(viewStore.count)") }
-
-        TcaProfileView(store: self.store)
-          .tabItem { Text("Profile \(viewStore.favorites.count)") }
-      }
-    }
   }
 }
 
 struct TcaCounterView: View {
-  let store: Store<AppState, AppAction>
+  let store: Store<CounterState, CounterAction>
   
   var body: some View {
     WithViewStore(self.store) { viewStore in
@@ -78,8 +145,24 @@ struct TcaCounterView: View {
   }
 }
 
+struct ProfileState: Equatable {
+  var favorites: Set<Int> = []
+}
+enum ProfileAction {
+  case removeButtonTapped(Int)
+}
+struct ProfileEnvironment {}
+
+let profileReducer = Reducer<ProfileState, ProfileAction, ProfileEnvironment> { state, action, _ in
+  switch action {
+  case let .removeButtonTapped(number):
+    state.favorites.remove(number)
+    return .none
+  }
+}
+
 struct TcaProfileView: View {
-  let store: Store<AppState, AppAction>
+  let store: Store<ProfileState, ProfileAction>
   
   var body: some View {
     WithViewStore(self.store) { viewStore in
@@ -89,7 +172,7 @@ struct TcaProfileView: View {
             Text("\(number)")
             Spacer()
             Button("Remove") {
-              viewStore.send(.profileRemoveButtonTapped(number))
+              viewStore.send(.removeButtonTapped(number))
             }
           }
         }
