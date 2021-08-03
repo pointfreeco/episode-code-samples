74bab22af7cfed9dca5370470a5fdbdd5d26a300
Point-Free <pointfreeco@Stephens-MBP.home>
2021-08-03 11:08:42 -0400

    wip

--- xyzw-search/Search/Search/SearchApp.swift
+++ xyzw-search/Search/Search/SearchApp.swift
@@ -1,10 +1,19 @@
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct SearchApp: App {
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      NavigationView {
+        ContentView(
+          store: .init(
+            initialState: .init(),
+            reducer: appReducer.debugActions(),
+            environment: .init()
+          )
+        )
+      }
     }
   }
 }
