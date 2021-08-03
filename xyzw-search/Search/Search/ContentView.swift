f73eb3247965fbdebfb5bf5cb4135b8019b9f9a9
Point-Free <point-free@brandons-mbp-2.myfiosgateway.com>
2021-08-03 10:39:38 -0400

    wip

--- xyzw-search/Search/Search/ContentView.swift
+++ xyzw-search/Search/Search/ContentView.swift
@@ -1,14 +1,41 @@
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    Map(
+      coordinateRegion: .constant(
+        .init(
+          center: .init(latitude: 40.7, longitude: -74),
+          span: .init(latitudeDelta: 0.075, longitudeDelta: 0.075)
+        )
+      )
+//      interactionModes: <#T##MapInteractionModes#>,
+//      showsUserLocation: <#T##Bool#>,
+//      userTrackingMode: <#T##Binding<MapUserTrackingMode>?#>,
+//      annotationItems: <#T##RandomAccessCollection#>,
+//      annotationContent: <#T##(Identifiable) -> MapAnnotationProtocol#>
+    )
+      .searchable(
+        text: .constant("")
+//        placement: <#T##SearchFieldPlacement#>,
+//        prompt: <#T##LocalizedStringKey#>,
+//        suggestions: <#T##() -> View#>
+      ) {
+        Text("Apple Store")
+        Text("Cafe")
+        Text("Library")
+      }
+      .navigationTitle("Places")
+      .navigationBarTitleDisplayMode(.inline)
+      .ignoresSafeArea(edges: .bottom)
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    NavigationView {
+      ContentView()
+    }
   }
 }
