ed6e8cb1dc547427da355a90c151151b4c3aa7ab
Point-Free <pointfreeco@Stephens-MBP.home>
2021-09-16 16:48:56 -0400

    wip

--- xyzw-swiftui-nav/SwiftUINavigation/SwiftUINavigation/ItemRow.swift
+++ xyzw-swiftui-nav/SwiftUINavigation/SwiftUINavigation/ItemRow.swift
@@ -1,27 +1,47 @@
+import CasePaths
 import SwiftUI
 
 class ItemRowViewModel: Identifiable, ObservableObject {
-  @Published var deleteItemAlertIsPresented: Bool
   @Published var item: Item
+  @Published var route: Route?
+  
+  enum Route {
+    case deleteAlert
+    case duplicate(Item)
+    case edit(Item)
+  }
 
   var onDelete: () -> Void = {}
 
   var id: Item.ID { self.item.id }
 
   init(
-    deleteItemAlertIsPresented: Bool = false,
-    item: Item
+    item: Item,
+    route: Route? = nil
   ) {
-    self.deleteItemAlertIsPresented = deleteItemAlertIsPresented
     self.item = item
+    self.route = route
   }
 
   func deleteButtonTapped() {
-    self.deleteItemAlertIsPresented = true
+    self.route = .deleteAlert
   }
 
   func deleteConfirmationButtonTapped() {
     self.onDelete()
+  }
+  
+  func editButtonTapped() {
+    self.route = .edit(self.item)
+  }
+  
+  func edit(item: Item) {
+    self.item = item
+    self.route = nil
+  }
+  
+  func cancelButtonTapped() {
+    self.route = nil
   }
 }
 
@@ -50,6 +70,11 @@ struct ItemRowView: View {
           .border(Color.black, width: 1)
       }
 
+      Button(action: { self.viewModel.editButtonTapped() }) {
+        Image(systemName: "pencil")
+      }
+      .padding(.leading)
+
       Button(action: { self.viewModel.deleteButtonTapped() }) {
         Image(systemName: "trash.fill")
       }
@@ -59,7 +84,7 @@ struct ItemRowView: View {
     .foregroundColor(self.viewModel.item.status.isInStock ? nil : Color.gray)
     .alert(
       self.viewModel.item.name,
-      isPresented: self.$viewModel.deleteItemAlertIsPresented,
+      isPresented: self.$viewModel.route.isPresent(/ItemRowViewModel.Route.deleteAlert),
       actions: {
         Button("Delete", role: .destructive) {
           self.viewModel.deleteConfirmationButtonTapped()
@@ -69,5 +94,23 @@ struct ItemRowView: View {
         Text("Are you sure you want to delete this item?")
       }
     )
+    .sheet(unwrap: self.$viewModel.route.case(/ItemRowViewModel.Route.edit)) { $item in
+      NavigationView {
+        ItemView(item: $item)
+          .navigationBarTitle("Edit")
+          .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+              Button("Cancel") {
+                self.viewModel.cancelButtonTapped()
+              }
+            }
+            ToolbarItem(placement: .primaryAction) {
+              Button("Save") {
+                self.viewModel.edit(item: item)
+              }
+            }
+          }
+      }
+    }
   }
 }
--- xyzw-swiftui-nav/SwiftUINavigation/SwiftUINavigation/SwiftUIHelpers.swift
+++ xyzw-swiftui-nav/SwiftUINavigation/SwiftUINavigation/SwiftUIHelpers.swift
@@ -13,6 +13,44 @@ extension Binding {
       }
     )
   }
+  
+  func isPresent<Enum, Case>(_ casePath: CasePath<Enum, Case>) -> Binding<Bool>
+  where Value == Enum? {
+    Binding<Bool>(
+      get: {
+        if let wrappedValue = self.wrappedValue, casePath.extract(from: wrappedValue) != nil {
+          return true
+        } else {
+          return false
+        }
+      },
+      set: { isPresented in
+        if !isPresented {
+          self.wrappedValue = nil
+        }
+      }
+    )
+  }
+  
+  func `case`<Enum, Case>(_ casePath: CasePath<Enum, Case>) -> Binding<Case?>
+  where Value == Enum? {
+    Binding<Case?>(
+      get: {
+        guard
+          let wrappedValue = self.wrappedValue,
+          let `case` = casePath.extract(from: wrappedValue)
+        else { return nil }
+        return `case`
+      },
+      set: { `case` in
+        if let `case` = `case` {
+          self.wrappedValue = casePath.embed(`case`)
+        } else {
+          self.wrappedValue = nil
+        }
+      }
+    )
+  }
 }
 
 extension View {
