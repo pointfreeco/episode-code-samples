4f15e9c18338bcb98b92fa23d561bb2f84768bf4
Point-Free <pointfreeco@Stephens-MBP.home>
2021-10-29 11:03:32 -0400

    wip

--- xyzw-swiftui-nav/SwiftUINavigation/SwiftUINavigation/ItemView.swift
+++ xyzw-swiftui-nav/SwiftUINavigation/SwiftUINavigation/ItemView.swift
@@ -1,20 +1,110 @@
 import CasePaths
 import SwiftUI
 
+struct ColorPickerView: View {
+  @Binding var color: Item.Color?
+  @State var newColors: [Item.Color] = []
+  @Environment(\.dismiss) var dismiss
+  
+  var body: some View {
+    Form {
+      Button(action: {
+        self.color = nil
+        self.dismiss()
+      }) {
+        HStack {
+          Text("None")
+          Spacer()
+          if self.color == nil {
+            Image(systemName: "checkmark")
+          }
+        }
+      }
+      
+      Section(header: Text("Default colors")) {
+        ForEach(Item.Color.defaults, id: \.name) { color in
+          Button(action: {
+            self.color = color
+            self.dismiss()
+          }) {
+            HStack {
+              Text(color.name)
+              Spacer()
+              if self.color == color {
+                Image(systemName: "checkmark")
+              }
+            }
+          }
+        }
+      }
+      
+      if !self.newColors.isEmpty {
+        Section(header: Text("New colors")) {
+          ForEach(self.newColors, id: \.name) { color in
+            Button(action: {
+              self.color = color
+              self.dismiss()
+            }) {
+              HStack {
+                Text(color.name)
+                Spacer()
+                if self.color == color {
+                  Image(systemName: "checkmark")
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    .task {
+      await Task.sleep(NSEC_PER_MSEC * 500)
+      self.newColors = [
+        .init(name: "Pink", red: 1, green: 0.7, blue: 0.7),
+      ]
+    }
+  }
+}
+
 struct ItemView: View {
   @Binding var item: Item
+  @State var nameIsDuplicate = false
 
   var body: some View {
     Form {
       TextField("Name", text: self.$item.name)
+        .background(self.nameIsDuplicate ? Color.red.opacity(0.1) : Color.clear)
+        .onChange(of: self.item.name) { newName in
+          // TODO: Validation logic
+          Task { @MainActor in
+            await Task.sleep(NSEC_PER_MSEC * 300)
+            self.nameIsDuplicate = newName == "Keyboard"
+          }
+        }
 
-      Picker(selection: self.$item.color, label: Text("Color")) {
-        Text("None")
-          .tag(Item.Color?.none)
-
-        ForEach(Item.Color.defaults, id: \.name) { color in
-          Text(color.name)
-            .tag(Optional(color))
+//      Picker(selection: self.$item.color, label: Text("Color")) {
+//        Text("None")
+//          .tag(Item.Color?.none)
+//
+//        ForEach(Item.Color.defaults, id: \.name) { color in
+//          Text(color.name)
+//            .tag(Optional(color))
+//        }
+//      }
+      NavigationLink(
+        destination: ColorPickerView(color: self.$item.color)
+      ) {
+        HStack {
+          Text("Color")
+          Spacer()
+          if let color = self.item.color {
+            Rectangle()
+              .frame(width: 30, height: 30)
+              .foregroundColor(color.swiftUIColor)
+              .border(Color.black, width: 1)
+          }
+          Text(self.item.color?.name ?? "None")
+            .foregroundColor(.gray)
         }
       }
       
