19c19f8cb92b20b1c503176cc51554bbf652fa55
Point-Free <pointfreeco@Stephens-MBP.home>
2021-11-22 13:58:05 -0500

    wip

---
+++ xyzw-swiftui-nav/SwiftUINavigation/SwiftUINavigation/ItemRowCellView.swift
@@ -0,0 +1,83 @@
+import Combine
+import UIKit
+
+class ItemRowCellView: UICollectionViewListCell {
+  var cancellables: Set<AnyCancellable> = []
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    self.cancellables = []
+  }
+  
+  func bind(viewModel: ItemRowViewModel, context: UIViewController) {
+    viewModel.$item
+      .map(\.name)
+      .removeDuplicates()
+      .sink { [unowned self] name in
+        var content = self.defaultContentConfiguration()
+        content.text = name
+        self.contentConfiguration = content
+      }
+      .store(in: &self.cancellables)
+    
+    viewModel.$route
+      .removeDuplicates()
+      .sink { [unowned self] route in
+        switch route {
+        case .none:
+          break
+        case .deleteAlert:
+          let alert = UIAlertController(
+            title: viewModel.item.name,
+            message: "Are you sure you want to delete this item?",
+            preferredStyle: .alert
+          )
+          alert.addAction(.init(title: "Cancel", style: .cancel, handler: { _ in
+            viewModel.cancelButtonTapped()
+          }))
+          alert.addAction(.init(title: "Delete", style: .destructive, handler: { _ in
+            viewModel.deleteConfirmationButtonTapped()
+          }))
+          context.present(alert, animated: true)
+
+        case let .duplicate(itemViewModel):
+          let vc = ItemViewController(viewModel: itemViewModel)
+          vc.title = "Duplicate"
+          vc.navigationItem.leftBarButtonItem = .init(
+            title: "Cancel",
+            primaryAction: .init { _ in
+              viewModel.cancelButtonTapped()
+            }
+          )
+          vc.navigationItem.rightBarButtonItem = .init(
+            title: "Add",
+            primaryAction: .init { _ in
+              viewModel.duplicate(item: itemViewModel.item)
+            }
+          )
+          let nav = UINavigationController(rootViewController: vc)
+          nav.modalPresentationStyle = .popover
+          nav.popoverPresentationController?.sourceView = self
+          context.present(nav, animated: true)
+
+        case let .edit(itemViewModel):
+          let vc = ItemViewController(viewModel: itemViewModel)
+          vc.title = "Edit"
+          vc.navigationItem.leftBarButtonItem = .init(
+            title: "Cancel",
+            primaryAction: .init { _ in
+              viewModel.cancelButtonTapped()
+            }
+          )
+          vc.navigationItem.rightBarButtonItem = .init(
+            title: "Save",
+            primaryAction: .init { _ in
+              viewModel.edit(item: itemViewModel.item)
+            }
+          )
+          context.show(vc, sender: nil)
+        }
+      }
+      .store(in: &self.cancellables)
+  }
+}
