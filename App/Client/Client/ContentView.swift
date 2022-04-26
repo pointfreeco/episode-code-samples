14cdc108b8ea639801bf370fd3467b52a40c998c
Point-Free <pointfreeco@Stephens-M1-Pro.home>
2022-04-26 13:41:51 -0400

    wip

--- App/Client/Client/ContentView.swift
+++ App/Client/Client/ContentView.swift
@@ -2,25 +2,52 @@ import SwiftUI
 import SiteRouter
 import _URLRouting
 
-let apiClient = URLRoutingClient.live(router: router.baseURL("http://127.0.0.1:8080"))
+class ViewModel: ObservableObject {
+  @Published var books: [BooksResponse.Book] = []
+  @Published var direction: SearchOptions.Direction = .asc
+
+  let apiClient: URLRoutingClient<SiteRoute>
+
+  init(apiClient: URLRoutingClient<SiteRoute>) {
+    self.apiClient = apiClient
+  }
+
+  @MainActor
+  func fetch() async {
+    do {
+      self.books = try await self.apiClient.request(
+        .users(.user(42, .books())),
+        as: BooksResponse.self
+      ).value.books
+    } catch {
+
+    }
+  }
+}
 
 struct ContentView: View {
-  @State var books: [BooksResponse.Book] = []
+  @ObservedObject var viewModel: ViewModel
 
   var body: some View {
-    List {
-      ForEach(self.books, id: \.id) { book in
-        Text(book.title)
+    VStack {
+      Picker(selection: self.$viewModel.direction) {
+        Text("Ascending").tag(SearchOptions.Direction.asc)
+        Text("Descending").tag(SearchOptions.Direction.desc)
+      } label: {
+        Text("Direction")
+      }
+      .pickerStyle(.segmented)
+
+      List {
+        ForEach(self.viewModel.books, id: \.id) { book in
+          Text(book.title)
+        }
       }
     }
-    .task {
-      do {
-        self.books = try await apiClient.request(
-          .users(.user(42, .books())),
-          as: BooksResponse.self
-        ).value.books
-      } catch {
-
+    .task { await self.viewModel.fetch() }
+    .onChange(of: self.viewModel.direction) { _ in
+      Task {
+        await self.viewModel.fetch()
       }
     }
   }
@@ -28,6 +55,23 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    ContentView(
+      viewModel: ViewModel(
+        apiClient: .failing
+          .override {
+            guard case .users(.user(_, .books(.search))) = $0
+            else { return false }
+            return true
+          } with: {
+            try .ok(
+              BooksResponse(
+                books: (1...100).map { n in
+                    .init(id: .init(), title: "Book \(n)", bookURL: URL(string: "/books/\(n)")!)
+                }
+              )
+            )
+          }
+      )
+    )
   }
 }
--- App/Client/ClientTests/ClientTests.swift
+++ App/Client/ClientTests/ClientTests.swift
@@ -1,21 +1,37 @@
-//
-//  ClientTests.swift
-//  ClientTests
-//
-//  Created by Point-Free on 4/21/22.
-//
-
 import XCTest
 @testable import Client
+import SiteRouter
 
 class ClientTests: XCTestCase {
+  func testBasics() async {
+    let viewModel = ViewModel(
+      apiClient: .failing
+        .override(.users(.user(42, .books(.search(.init(direction: .asc)))))) {
+          try .ok(
+            BooksResponse(
+              books: [
+                .init(
+                  id: .init(uuidString: "deadbeef-dead-beef-dead-beefdeadbeef")!,
+                  title: "Blobbed around the world",
+                  bookURL: URL(string: "/books/deadbeef-dead-beef-dead-beefdeadbeef")!
+                )
+              ]
+            )
+          )
+        }
+    )
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    await viewModel.fetch()
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    XCTAssertEqual(
+      viewModel.books,
+      [
+        .init(
+          id: .init(uuidString: "deadbeef-dead-beef-dead-beefdeadbeef")!,
+          title: "Blobbed around the world",
+          bookURL: URL(string: "/books/deadbeef-dead-beef-dead-beefdeadbeef")!
+        )
+      ]
+    )
+  }
 }
