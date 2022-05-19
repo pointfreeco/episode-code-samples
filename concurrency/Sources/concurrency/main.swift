ae2c0e02bcc12c16d8ec62befcda9deeaf531d79
Point-Free <pointfreeco@Stephens-M1-Pro.home>
2022-05-19 13:49:16 -0400

    wip

--- concurrency/Sources/concurrency/main.swift
+++ concurrency/Sources/concurrency/main.swift
@@ -1,19 +1,37 @@
 import Foundation
 
-Thread.detachNewThread {
-  print("1", Thread.current)
+func threadBasics() {
+  Thread.detachNewThread {
+    print("1", Thread.current)
+  }
+  Thread.detachNewThread {
+    print("2", Thread.current)
+  }
+  Thread.detachNewThread {
+    print("3", Thread.current)
+  }
+  Thread.detachNewThread {
+    print("4", Thread.current)
+  }
+  Thread.detachNewThread {
+    print("5", Thread.current)
+  }
 }
-Thread.detachNewThread {
-  print("2", Thread.current)
+
+let thread = Thread {
+  let start = Date()
+  defer { print("Finished in", Date().timeIntervalSince(start)) }
+  Thread.sleep(forTimeInterval: 1)
+  guard !Thread.current.isCancelled
+  else {
+    print("Cancelled!")
+    return
+  }
+  print(Thread.current)
 }
-Thread.detachNewThread {
-  print("3", Thread.current)
-}
-Thread.detachNewThread {
-  print("4", Thread.current)
-}
-Thread.detachNewThread {
-  print("5", Thread.current)
-}
+thread.threadPriority = 0.75
+thread.start()
+Thread.sleep(forTimeInterval: 0.01)
+thread.cancel()
 
 Thread.sleep(forTimeInterval: 1.1)
