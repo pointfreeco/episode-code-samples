8d65a6446f217308e27db52fd1b971d62e28c1c6
Point-Free <pointfreeco@Stephens-M1-Pro.home>
2022-05-19 14:28:57 -0400

    wip

--- concurrency/Sources/concurrency/main.swift
+++ concurrency/Sources/concurrency/main.swift
@@ -23,6 +23,9 @@ func threadPriorityAndCancellation() {
     let start = Date()
     defer { print("Finished in", Date().timeIntervalSince(start)) }
     Thread.sleep(forTimeInterval: 1)
+    Thread.detachNewThread {
+      print("Inner thread cancelled?", Thread.current.isCancelled)
+    }
     guard !Thread.current.isCancelled
     else {
       print("Cancelled!")
@@ -36,38 +39,49 @@ func threadPriorityAndCancellation() {
   thread.cancel()
 }
 
-func makeDatabaseQuery() {
-  let requestId = Thread.current.threadDictionary["requestId"] as! UUID
-  print(requestId, "Making database query")
-  Thread.sleep(forTimeInterval: 0.5)
-  print(requestId, "Finished database query")
-}
-func makeNetworkRequest() {
-  let requestId = Thread.current.threadDictionary["requestId"] as! UUID
-  print(requestId, "Making network request")
-  Thread.sleep(forTimeInterval: 0.5)
-  print(requestId, "Finished network request")
-}
-
-func response(for request: URLRequest) -> HTTPURLResponse {
-  let requestId = Thread.current.threadDictionary["requestId"] as! UUID
-
-  let start = Date()
-  defer { print(requestId, "Finished in", Date().timeIntervalSince(start)) }
-
-  makeDatabaseQuery()
-  makeNetworkRequest()
-
-  return .init()
-}
-
-for _ in 0..<10 {
-  let thread = Thread {
-    response(for: .init(url: .init(string: "http://pointfree.co")!))
+func threadStorageAndCoordination() {
+  func makeDatabaseQuery() {
+    let requestId = Thread.current.threadDictionary["requestId"] as! UUID
+    print(requestId, "Making database query")
+    Thread.sleep(forTimeInterval: 0.5)
+    print(requestId, "Finished database query")
   }
-  thread.threadDictionary["requestId"] = UUID()
-  thread.start()
+  func makeNetworkRequest() {
+    let requestId = Thread.current.threadDictionary["requestId"] as! UUID
+    print(requestId, "Making network request")
+    Thread.sleep(forTimeInterval: 0.5)
+    print(requestId, "Finished network request")
+  }
+
+  func response(for request: URLRequest) -> HTTPURLResponse {
+    let requestId = Thread.current.threadDictionary["requestId"] as! UUID
+
+    let start = Date()
+    defer { print(requestId, "Finished in", Date().timeIntervalSince(start)) }
+
+    let databaseQueryThread = Thread { makeDatabaseQuery() }
+    databaseQueryThread.threadDictionary.addEntries(from: Thread.current.threadDictionary as! [AnyHashable: Any])
+    databaseQueryThread.start()
+    let networkRequestThread = Thread { makeNetworkRequest() }
+    networkRequestThread.threadDictionary.addEntries(from: Thread.current.threadDictionary as! [AnyHashable: Any])
+    networkRequestThread.start()
+
+    while !databaseQueryThread.isFinished || !networkRequestThread.isFinished {
+      Thread.sleep(forTimeInterval: 0.1)
+    }
+
+    return .init()
+  }
+
+  //for _ in 0..<10 {
+    let thread = Thread {
+      response(for: .init(url: .init(string: "http://pointfree.co")!))
+    }
+    thread.threadDictionary["requestId"] = UUID()
+    thread.start()
+  //}
 }
 
+threadPriorityAndCancellation()
 
 Thread.sleep(forTimeInterval: 1.1)
