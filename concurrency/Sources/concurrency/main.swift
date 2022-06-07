2f14bc7e4eb02774624278ccf7a28b30e1d9f2cc
Point-Free <pointfreeco@Stephens-M1-Pro.home>
2022-06-07 15:17:15 -0400

    wip

--- concurrency/Sources/concurrency/main.swift
+++ concurrency/Sources/concurrency/main.swift
@@ -65,20 +65,101 @@ func taskPriority() {
   }
 }
 
-func doSomething() async throws {
-  try await Task.sleep(nanoseconds: NSEC_PER_SEC)
+func taskCancellation() {
+  func doSomething() async throws {
+    try await Task.sleep(nanoseconds: NSEC_PER_SEC)
+  }
+
+  let task = Task {
+    let start = Date()
+    defer { print("Task finished in", Date().timeIntervalSince(start)) }
+
+    let (data, _) = try await URLSession.shared.data(from: .init(string: "http://ipv4.download.thinkbroadband.com/1MB.zip")!)
+    print(Thread.current, "network request finished", data.count)
+  }
+
+  Thread.sleep(forTimeInterval: 0.5)
+  task.cancel()
 }
 
-let task = Task {
-  let start = Date()
-  defer { print("Task finished in", Date().timeIntervalSince(start)) }
 
-  let (data, _) = try await URLSession.shared.data(from: .init(string: "http://ipv4.download.thinkbroadband.com/1MB.zip")!)
-  print(Thread.current, "network request finished", data.count)
+enum RequestData {
+  @TaskLocal static var requestId: UUID!
+  @TaskLocal static var startDate: Date!
 }
 
-Thread.sleep(forTimeInterval: 0.5)
-task.cancel()
+
+func databaseQuery() async throws {
+  let requestId = RequestData.requestId!
+  print(requestId, "Making database query")
+  try await Task.sleep(nanoseconds: 500_000_000)
+  print(requestId, "Finished database query")
+}
+
+func networkRequest() async throws {
+  let requestId = RequestData.requestId!
+  print(requestId, "Making network request")
+  try await Task.sleep(nanoseconds: 500_000_000)
+  print(requestId, "Finished network request")
+}
+
+
+func response(for request: URLRequest) async throws -> HTTPURLResponse {
+  let requestId = RequestData.requestId!
+  let start = RequestData.startDate!
+
+  defer { print(requestId, "Request finished in", Date().timeIntervalSince(start)) }
+
+  Task {
+    print(RequestData.requestId!, "Track analytics")
+  }
+  try await databaseQuery()
+  try await networkRequest()
+
+  // TODO: return real response
+  return .init()
+}
+
+
+RequestData.$requestId.withValue(UUID()) {
+  RequestData.$startDate.withValue(Date()) {
+    Task {
+      _ = try await response(for: .init(url: .init(string: "https://www.pointfree.co")!))
+    }
+  }
+}
+
+
+//enum MyLocals {
+//  @TaskLocal static var id: Int!
+//
+//  // @TaskLocal var api: APIClient
+//  // @TaskLocal var database: DatabaseClient
+//  // @TaskLocal var stripe: StripeClient
+//}
+//
+//func doSomething() async {
+//  print("doSomething:", MyLocals.id!)
+//}
+//
+//print("before:", MyLocals.id)
+//MyLocals.$id.withValue(42) {
+//  print("withValue:", MyLocals.id!)
+//  Task {
+//    MyLocals.$id.withValue(1729) {
+//      Task {
+//        try await Task.sleep(nanoseconds: 2 * NSEC_PER_SEC)
+//        print("Task 2:", MyLocals.id!)
+//      }
+//    }
+//    try await Task.sleep(nanoseconds: NSEC_PER_SEC)
+//    Task {
+//      print("Task:", MyLocals.id!)
+//      await doSomething()
+//    }
+//  }
+//}
+//print("after:", MyLocals.id)
 
 
 Thread.sleep(forTimeInterval: 5)
