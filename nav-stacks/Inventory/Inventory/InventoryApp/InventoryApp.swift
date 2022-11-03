8cd618341d6ecfed0152b10bdbf68869654cce72
Point-Free <pointfreeco@Stephens-M1-Pro.local>
2022-11-03 12:54:00 -0700

    wip

--- nav-stacks/Inventory/Inventory/InventoryApp/InventoryApp.swift
+++ nav-stacks/Inventory/Inventory/InventoryApp/InventoryApp.swift
@@ -5,19 +5,52 @@ import ItemRowFeature
 import Models
 import SwiftUI
 
+
+class NestedModel: ObservableObject {
+  @Published var child: NestedModel?
+  init(child: NestedModel? = nil) {
+    self.child = child
+  }
+}
+
+
+struct NestedView: View {
+  @ObservedObject var model: NestedModel
+
+  var body: some View {
+    NavigationLink(
+      unwrapping: self.$model.child
+    ) { isActive in
+      self.model.child = isActive ? NestedModel() : nil
+    } destination: { $child in
+      NestedView(model: child)
+    } label: {
+      Text("Go to child feature")
+    }
+  }
+}
+
+
+let lastItem = Item(
+  name: "Headphones",
+  color: .red,
+  status: .outOfStock(isOnBackOrder: false)
+)
+
+
 @main
 struct InventoryApp: App {
   let model = AppModel(
     inventoryModel: InventoryModel(
-      destination: .add(
-        ItemModel(
-          destination: .colorPicker,
-          item: Item(
-            name: "Ergnomic Keyboard",
-            status: .inStock(quantity: 100)
-          )
-        )
-      ),
+//      destination: .add(
+//        ItemModel(
+//          destination: .colorPicker,
+//          item: Item(
+//            name: "Ergnomic Keyboard",
+//            status: .inStock(quantity: 100)
+//          )
+//        )
+//      ),
       inventory: [
         ItemRowModel(
           item: Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
@@ -31,6 +64,118 @@ struct InventoryApp: App {
         ItemRowModel(
           item: Item(name: "Headphones", color: .red, status: .outOfStock(isOnBackOrder: false))
         ),
+        ItemRowModel(
+          item: Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
+        ),
+        ItemRowModel(
+          item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))
+        ),
+        ItemRowModel(
+          item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))
+        ),
+        ItemRowModel(
+          item: Item(name: "Headphones", color: .red, status: .outOfStock(isOnBackOrder: false))
+        ),
+        ItemRowModel(
+          item: Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
+        ),
+        ItemRowModel(
+          item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))
+        ),
+        ItemRowModel(
+          item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))
+        ),
+        ItemRowModel(
+          item: Item(name: "Headphones", color: .red, status: .outOfStock(isOnBackOrder: false))
+        ),
+        ItemRowModel(
+          item: Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
+        ),
+        ItemRowModel(
+          item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))
+        ),
+        ItemRowModel(
+          item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))
+        ),
+        ItemRowModel(
+          item: Item(name: "Headphones", color: .red, status: .outOfStock(isOnBackOrder: false))
+        ),
+        ItemRowModel(
+          item: Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
+        ),
+        ItemRowModel(
+          item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))
+        ),
+        ItemRowModel(
+          item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))
+        ),
+        ItemRowModel(
+          item: Item(name: "Headphones", color: .red, status: .outOfStock(isOnBackOrder: false))
+        ),
+        ItemRowModel(
+          item: Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
+        ),
+        ItemRowModel(
+          item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))
+        ),
+        ItemRowModel(
+          item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))
+        ),
+        ItemRowModel(
+          item: Item(name: "Headphones", color: .red, status: .outOfStock(isOnBackOrder: false))
+        ),
+        ItemRowModel(
+          item: Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
+        ),
+        ItemRowModel(
+          item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))
+        ),
+        ItemRowModel(
+          item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))
+        ),
+        ItemRowModel(
+          item: Item(name: "Headphones", color: .red, status: .outOfStock(isOnBackOrder: false))
+        ),
+        ItemRowModel(
+          item: Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
+        ),
+        ItemRowModel(
+          item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))
+        ),
+        ItemRowModel(
+          item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))
+        ),
+        ItemRowModel(
+          item: Item(name: "Headphones", color: .red, status: .outOfStock(isOnBackOrder: false))
+        ),
+        ItemRowModel(
+          item: Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
+        ),
+        ItemRowModel(
+          item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))
+        ),
+        ItemRowModel(
+          item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))
+        ),
+        ItemRowModel(
+          item: Item(name: "Headphones", color: .red, status: .outOfStock(isOnBackOrder: false))
+        ),
+        ItemRowModel(
+          item: Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
+        ),
+        ItemRowModel(
+          item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))
+        ),
+        ItemRowModel(
+          item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))
+        ),
+        ItemRowModel(
+          item: Item(name: "Headphones", color: .red, status: .outOfStock(isOnBackOrder: false))
+        ),
+        ItemRowModel(
+          destination: .edit(ItemModel(item: lastItem)),
+          item: lastItem
+        )
       ]
     ),
     selectedTab: .inventory
@@ -44,6 +189,52 @@ struct InventoryApp: App {
         .onOpenURL { url in
           self.model.open(url: url)
         }
+
+//      NavigationView {
+//        NestedView(
+//          model: NestedModel(
+//            child: NestedModel(
+//              child: NestedModel(
+//                child: NestedModel(
+//                  child: NestedModel(
+//                    child: NestedModel(
+//                      child: .init()
+//                    )
+//                  )
+//                )
+//              )
+//            )
+//          )
+//        )
+//      }
+//      .navigationViewStyle(.stack)
     }
   }
 }
+
+
+
+/*
+                                 |-------|
+                                 |   U   |
+                 |-------|       |   s   |
+                 |   I   |       |   e   |-------|
+         |-------|   n   |       |   r   |   S   |
+         |   I   |   v   |-------|   P   |   e   |
+         |   t   |   e   |   S   |   r   |   t   |
+ |-------|   e   |   n   |   e   |   o   |   t   |
+ |   I   |   m   |   t   |   a   |   f   |   i   |    ...
+ |   t   |   R   |   o   |   r   |   i   |   n   |
+ |   e   |   o   |   r   |   c   |   l   |   g   |
+ |   m   |   w   |   y   |   h   |   e   |   s   |
+ |-------|-------|-------|-------|-------|------*/
+/*--------------------------------------------------------|
+|                                                         |
+|            MODEL/HELPER/DEPENDENCY MODULES              |
+|                                                         |
+|      |---------------|-----------|---------------|      |
+|      |    Models     | ApiClient | ApiClientLive |      |
+|      |---------------|-----------|---------------|      |
+|                                                         |
+|---------------------------------------------------------|
+*/
