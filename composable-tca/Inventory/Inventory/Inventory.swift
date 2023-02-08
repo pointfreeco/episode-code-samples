8bc3a3d6f645c8ba008c4acf18e17915a990ac1a
Point-Free <pointfreeco@Stephens-M1-Pro.lan>
2023-02-08 15:27:13 -0800

    wip

--- composable-tca/Inventory/Inventory/Inventory.swift
+++ composable-tca/Inventory/Inventory/Inventory.swift
@@ -2,10 +2,45 @@ import ComposableArchitecture
 import SwiftUI
 
 struct InventoryFeature: Reducer {
-  struct State: Equatable {}
-  enum Action: Equatable {}
+  struct State: Equatable {
+    var alert: AlertState<Action.Alert>?
+    var items: IdentifiedArrayOf<Item> = []
+  }
+  enum Action: Equatable {
+    case alert(Alert)
+    case deleteButtonTapped(id: Item.ID)
+
+    enum Alert: Equatable {
+      case confirmDeletion(id: Item.ID)
+      case dismiss
+    }
+  }
 
   func reduce(into state: inout State, action: Action) -> Effect<Action> {
+    switch action {
+    case let .alert(.confirmDeletion(id)):
+      state.items.remove(id: id)
+      return .none
+
+    case .alert(.dismiss):
+      state.alert = nil
+      return .none
+
+    case let .deleteButtonTapped(id):
+      guard let item = state.items[id: id]
+      else { return .none }
+
+      state.alert = AlertState {
+        TextState(#"Delete "\#(item.name)""#)
+      } actions: {
+        ButtonState(role: .destructive, action: .send(.confirmDeletion(id: item.id), animation: .default)) {
+          TextState("Delete")
+        }
+      } message: {
+        TextState("Are you sure you want to delete this item?")
+      }
+      return .none
+    }
   }
 }
 
@@ -13,6 +48,65 @@ struct InventoryView: View {
   let store: StoreOf<InventoryFeature>
   
   var body: some View {
-    Text("Inventory")
+    WithViewStore(self.store, observe: \.items) { viewStore in
+      List {
+        ForEach(viewStore.state) { item in
+          HStack {
+            VStack(alignment: .leading) {
+              Text(item.name)
+
+              switch item.status {
+              case let .inStock(quantity):
+                Text("In stock: \(quantity)")
+              case let .outOfStock(isOnBackOrder):
+                Text("Out of stock" + (isOnBackOrder ? ": on back order" : ""))
+              }
+            }
+
+            Spacer()
+
+            if let color = item.color {
+              Rectangle()
+                .frame(width: 30, height: 30)
+                .foregroundColor(color.swiftUIColor)
+                .border(Color.black, width: 1)
+            }
+
+            Button {
+              viewStore.send(.deleteButtonTapped(id: item.id))
+            } label: {
+              Image(systemName: "trash.fill")
+            }
+            .padding(.leading)
+          }
+          .buttonStyle(.plain)
+          .foregroundColor(item.status.isInStock ? nil : Color.gray)
+        }
+      }
+      .alert(
+        self.store.scope(state: \.alert, action: InventoryFeature.Action.alert),
+        dismiss: .dismiss
+      )
+    }
+  }
+}
+
+struct Inventory_Previews: PreviewProvider {
+  static var previews: some View {
+    NavigationStack {
+      InventoryView(
+        store: Store(
+          initialState: InventoryFeature.State(
+            items: [
+              .headphones,
+              .mouse,
+              .keyboard,
+              .monitor,
+            ]
+          ),
+          reducer: InventoryFeature()
+        )
+      )
+    }
   }
 }
