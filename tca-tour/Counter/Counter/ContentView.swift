230082b65a14c526fa3ad2091d4c183406cb1f15
Brandon Williams <mbrandonw@hey.com>
2023-07-20 14:14:10 -0400

    wip

--- tca-tour/Counter/Counter/ContentView.swift
+++ tca-tour/Counter/Counter/ContentView.swift
@@ -8,7 +8,7 @@ struct CounterFeature: Reducer {
     var isLoadingFact = false
     var isTimerOn = false
   }
-  enum Action {
+  enum Action: Equatable {
     case decrementButtonTapped
     case factResponse(String)
     case getFactButtonTapped
@@ -19,6 +19,8 @@ struct CounterFeature: Reducer {
   private enum CancelID {
     case timer
   }
+  @Dependency(\.continuousClock) var clock
+
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
@@ -36,7 +38,7 @@ struct CounterFeature: Reducer {
         state.fact = nil
         state.isLoadingFact = true
         return .run { [count = state.count] send in
-          try await Task.sleep(for: .seconds(1))
+          try await self.clock.sleep(for: .seconds(1))
           let (data, _) = try await URLSession.shared.data(
             from: URL(string: "http://www.numbersapi.com/\(count)")!
           )
@@ -57,8 +59,7 @@ struct CounterFeature: Reducer {
         state.isTimerOn.toggle()
         if state.isTimerOn {
           return .run { send in
-            while true {
-              try await Task.sleep(for: .seconds(1))
+            for await _ in self.clock.timer(interval: .seconds(1)) {
               await send(.timerTicked)
             }
           }
@@ -66,7 +67,6 @@ struct CounterFeature: Reducer {
         } else {
           return .cancel(id: CancelID.timer)
         }
-        return .none
       }
     }
   }
--- tca-tour/Counter/CounterTests/CounterTests.swift
+++ tca-tour/Counter/CounterTests/CounterTests.swift
@@ -1,5 +1,57 @@
+import ComposableArchitecture
 import XCTest
 @testable import Counter
 
+@MainActor
 final class CounterTests: XCTestCase {
+  func testCounter() async {
+    let store = TestStore(initialState: CounterFeature.State()) {
+      CounterFeature()
+    }
+
+    await store.send(.incrementButtonTapped) {
+      $0.count = 1
+    }
+  }
+
+  func testTimer() async throws {
+    let clock = TestClock()
+
+    let store = TestStore(initialState: CounterFeature.State()) {
+      CounterFeature()
+    } withDependencies: {
+      $0.continuousClock = clock
+    }
+
+    await store.send(.toggleTimerButtonTapped) {
+      $0.isTimerOn = true
+    }
+    await clock.advance(by: .seconds(1))
+    await store.receive(.timerTicked) {
+      $0.count = 1
+    }
+    await clock.advance(by: .seconds(1))
+    await store.receive(.timerTicked) {
+      $0.count = 2
+    }
+    await store.send(.toggleTimerButtonTapped) {
+      $0.isTimerOn = false
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
