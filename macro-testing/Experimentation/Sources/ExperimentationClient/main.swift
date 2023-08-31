8c69e37c647fc62884a792cd6da33d454948b33f
Point-Free <pointfreeco@Stephens-M1-Pro.lan>
2023-08-31 10:05:00 -0700

    wip

---
+++ macro-testing/Experimentation/Sources/ExperimentationClient/main.swift
@@ -0,0 +1,8 @@
+import Experimentation
+
+let a = 17
+let b = 25
+
+let (result, code) = #stringify(a + b)
+
+print("The value \(result) was produced by the code \"\(code)\"")
---
+++ macro-testing/Experimentation/Tests/ExperimentationTests/ExperimentationTests.swift
@@ -0,0 +1,46 @@
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+import XCTest
+
+// Macro implementations build for the host, so the corresponding module is not available when cross-compiling. Cross-compiled tests may still make use of the macro itself in end-to-end tests.
+#if canImport(ExperimentationMacros)
+import ExperimentationMacros
+
+let testMacros: [String: Macro.Type] = [
+    "stringify": StringifyMacro.self,
+]
+#endif
+
+final class ExperimentationTests: XCTestCase {
+    func testMacro() throws {
+        #if canImport(ExperimentationMacros)
+        assertMacroExpansion(
+            """
+            #stringify(a + b)
+            """,
+            expandedSource: """
+            (a + b, "a + b)
+            """,
+            macros: testMacros
+        )
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
+
+    func testMacroWithStringLiteral() throws {
+        #if canImport(ExperimentationMacros)
+        assertMacroExpansion(
+            #"""
+            #stringify("Hello, \(name)")
+            """#,
+            expandedSource: #"""
+            ("Hello, \(name)", #""Hello, \(name)""#)
+            """#,
+            macros: testMacros
+        )
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
+}
