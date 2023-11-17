6a7401ab9d11fe7d2d9c00dce275729aecb5b2c3
Brandon Williams <mbrandonw@hey.com>
2023-11-17 14:27:59 -0500

    wip

--- observable-architecture/SneakPeek/SneakPeek/ContentView.swift
+++ observable-architecture/SneakPeek/SneakPeek/ContentView.swift
@@ -6,10 +6,12 @@ struct CounterFeature {
   @ObservableState
   struct State {
     var count = 0
+    var isObservingCount = true
   }
   enum Action {
     case decrementButtonTapped
     case incrementButtonTapped
+    case toggleIsObservingCount
   }
   var body: some ReducerOf<Self> {
     Reduce { state, action in
@@ -20,6 +22,9 @@ struct CounterFeature {
       case .incrementButtonTapped:
         state.count += 1
         return .none
+      case .toggleIsObservingCount:
+        state.isObservingCount.toggle()
+        return .none
       }
     }
   }
@@ -29,11 +34,18 @@ struct ContentView: View {
   let store: StoreOf<CounterFeature>
 
   var body: some View {
-    Form {
-      Text(self.store.count.description)
-      Button("Decrement") { self.store.send(.decrementButtonTapped) }
-      Button("Increment") { self.store.send(.incrementButtonTapped) }
-    }
+    // WithViewStore(self.store, observe: ) { viewStore in
+    //WithPerceptionTracking {
+      let _ = Self._printChanges()
+      Form {
+        if self.store.isObservingCount {
+          Text(self.store.count.description)
+        }
+        Button("Decrement") { self.store.send(.decrementButtonTapped) }
+        Button("Increment") { self.store.send(.incrementButtonTapped) }
+        Button("Toggle count") { self.store.send(.toggleIsObservingCount) }
+      }
+    //}
   }
 }
 
