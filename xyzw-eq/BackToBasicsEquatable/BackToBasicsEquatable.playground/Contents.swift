53d8098c130da4d5467d4b90c338f42fe31d2d62
Point-Free <pointfreeco@Stephens-M2-Pro.local>
2024-09-30 11:28:50 -0700

    wip

---
+++ xyzw-eq/BackToBasicsEquatable/BackToBasicsEquatable.playground/Contents.swift
@@ -0,0 +1,131 @@
+//let x: any Equatable
+
+struct User: Equatable {
+  let id: Int
+  var isAdmin = false
+  var name: String
+
+  static func == (lhs: Self, rhs: Self) -> Bool {
+//    lhs.id == rhs.id && lhs.isAdmin == rhs.isAdmin && lhs.name == rhs.name
+    lhs.id == rhs.id
+//    lhs.isAdmin == rhs.isAdmin && lhs.name == rhs.name
+  }
+}
+
+let blob = User(id: 42, isAdmin: false, name: "Blob")
+
+let id42Partition = [
+  User(id: 42, isAdmin: true, name: "Blob"),
+  User(id: 42, isAdmin: false, name: "Blob, Esq."),
+  User(id: 42, isAdmin: true, name: "Blob, Esq."),
+  User(id: 42, isAdmin: true, name: "Blob, MD"),
+  User(id: 42, isAdmin: true, name: "Blob, MD"),
+  // ...
+]
+
+id42Partition.allSatisfy { lhs in
+  id42Partition.allSatisfy { rhs in
+    lhs == rhs
+  }
+}
+
+struct Mod12: Equatable {
+  var value: Int
+  init(_ value: Int) {
+    self.value = value
+  }
+
+  static func == (lhs: Self, rhs: Self) -> Bool {
+    (rhs.value - lhs.value).isMultiple(of: 12)
+  }
+}
+
+Mod12(1) == Mod12(6)
+Mod12(1) == Mod12(13)
+Mod12(1) == Mod12(25)
+
+struct NeverEqual<A>: Equatable {
+  var value: A
+  init(_ value: A) {
+    self.value = value
+  }
+  static func == (lhs: Self, rhs: Self) -> Bool { false }
+}
+
+do {
+  let neverEqual1 = NeverEqual(1)
+  neverEqual1 == neverEqual1
+
+  let values = [NeverEqual(true), NeverEqual(false)]
+  values.count
+  values.contains(NeverEqual(true))
+  values.contains(NeverEqual(false))
+}
+
+struct LessThanOrEqual: Equatable {
+  var value: Int
+  init(_ value: Int) {
+    self.value = value
+  }
+  static func == (lhs: Self, rhs: Self) -> Bool {
+    lhs.value <= rhs.value
+  }
+}
+
+LessThanOrEqual(2) == LessThanOrEqual(3)
+LessThanOrEqual(3) == LessThanOrEqual(2)
+
+extension Array where Element: Equatable {
+  func firstOffset(of needle: Element) -> Int? {
+    for (offset, element) in enumerated() {
+      if needle == element {
+//      if element == needle {
+        return offset
+      }
+    }
+    return nil
+  }
+}
+
+[
+  LessThanOrEqual(2),
+  LessThanOrEqual(3),
+]
+  .firstOffset(of: LessThanOrEqual(3))
+
+struct Approximation: Equatable {
+  var value: Double
+  init(_ value: Double) {
+    self.value = value
+  }
+  static func == (lhs: Self, rhs: Self) -> Bool {
+    abs(lhs.value - rhs.value) < 0.1
+  }
+}
+
+Approximation(1) == Approximation(1.05)
+Approximation(1.05) == Approximation(1.1)
+Approximation(1) == Approximation(1.1)
+
+extension Array where Element: Equatable {
+  func uniques() -> Array {
+    var result = Array()
+    for element in self {
+      if !result.contains(element) {
+        result.append(element)
+      }
+    }
+    return result
+  }
+}
+
+[
+  Approximation(1),
+  Approximation(1.05),
+  Approximation(1.025),
+  Approximation(1.075),
+  Approximation(1.1),
+  Approximation(1.15),
+]
+  .uniques()
+  .map(\.value)
---
+++ xyzw-eq/BackToBasicsEquatable/BackToBasicsEquatable/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  BackToBasicsEquatable
+//
+//  Created by Point-Free on 9/30/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
