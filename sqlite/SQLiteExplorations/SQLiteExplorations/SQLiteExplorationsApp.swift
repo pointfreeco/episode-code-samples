baf57008678cf19de9262d80c819ddf4357e9d73
Point-Free <pointfreeco@Mac.lan>
2024-10-31 11:03:41 -0700

    wip

--- sqlite/SQLiteExplorations/SQLiteExplorations/SQLiteExplorationsApp.swift
+++ sqlite/SQLiteExplorations/SQLiteExplorations/SQLiteExplorationsApp.swift
@@ -4,7 +4,8 @@ import SwiftUI
 @main
 struct SQLiteExplorationsApp: App {
   init() {
-    let databasePath = URL.documentsDirectory.appending(path: "db.sqlite").path()
+    let databasePath = URL.documentsDirectory.appending(path: "db.sqlite")
+      .path()
     print("open", databasePath)
     var db: OpaquePointer?
     guard
@@ -17,6 +18,22 @@ struct SQLiteExplorationsApp: App {
     else {
       fatalError("Could not open database at \(databasePath)")
     }
+    print(
+      "Create table",
+      sqlite3_exec(
+        db,
+        """
+        CREATE TABLE IF NOT EXISTS "players" (
+          "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+          "name" TEXT NOT NULL,
+          "createdAt" DATETIME NOT NULL
+        )
+        """,
+        nil,
+        nil,
+        nil
+      ) == SQLITE_OK
+    )
   }
 
   var body: some Scene {
