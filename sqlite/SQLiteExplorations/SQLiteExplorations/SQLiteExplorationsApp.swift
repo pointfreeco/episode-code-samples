3bb40d55005c2964ea89034e137e7c697216f1ed
Brandon Williams <mbrandonw@hey.com>
2024-10-31 14:24:50 -0400

    wip

--- sqlite/SQLiteExplorations/SQLiteExplorations/SQLiteExplorationsApp.swift
+++ sqlite/SQLiteExplorations/SQLiteExplorations/SQLiteExplorationsApp.swift
@@ -34,6 +34,49 @@ struct SQLiteExplorationsApp: App {
         nil
       ) == SQLITE_OK
     )
+    let name = "Blob"
+    let createdAt = Date()
+    var statement: OpaquePointer?
+    sqlite3_prepare_v2(
+      db,
+      """
+      INSERT INTO "players"
+      ("name", "createdAt") 
+      VALUES
+      (?, ?)
+      """,
+      -1,
+      &statement,
+      nil
+    )
+    let SQLITE_TRANSIENT = unsafeBitCast(-1, to: sqlite3_destructor_type.self)
+    print(
+      "Binding name",
+      sqlite3_bind_text(
+        statement,
+        1,
+        name,
+        -1,
+        SQLITE_TRANSIENT
+      ) == SQLITE_OK
+    )
+    print(
+      "Binding createdAt",
+      sqlite3_bind_int64(
+        statement,
+        2,
+        Int64(createdAt.timeIntervalSince1970)
+      ) == SQLITE_OK
+    )
+    print(
+      "Insert Blob",
+      sqlite3_step(statement) == SQLITE_DONE
+    )
+    print(
+      "Finalize insert",
+      sqlite3_finalize(statement) == SQLITE_OK
+    )
+
   }
 
   var body: some Scene {
