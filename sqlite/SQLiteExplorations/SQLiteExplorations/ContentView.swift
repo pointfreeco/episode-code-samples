305f10ed9b519fe6a2e735e8715b3f97369fb48b
Brandon Williams <mbrandonw@hey.com>
2024-11-06 15:35:14 -0500

    wip

--- sqlite/SQLiteExplorations/SQLiteExplorations/ContentView.swift
+++ sqlite/SQLiteExplorations/SQLiteExplorations/ContentView.swift
@@ -5,24 +5,83 @@ import SwiftUI
 struct ContentView: View {
   let databaseQueue: DatabaseQueue
   @State var players: [Player] = []
+  @State var playerDetail: Player?
 
   var body: some View {
     Form {
-      ForEach(players, id: \.id) { player in
-        HStack {
-          Text(player.name)
-          if player.isInjured {
-            Spacer()
-            Image(systemName: "stethoscope")
-              .foregroundStyle(.red)
+      ForEach(players) { player in
+        Button {
+          playerDetail = player
+        } label: {
+          HStack {
+            Text(player.name)
+            if player.isInjured {
+              Spacer()
+              Image(systemName: "stethoscope")
+                .foregroundStyle(.red)
+            }
           }
         }
       }
+    }
+    .sheet(item: $playerDetail) { player in
+      NavigationStack {
+        PlayerDetailView(
+          databaseQueue: databaseQueue,
+          player: player
+        )
+      }
+      .presentationDetents([.medium])
     }
     .onAppear {
       do {
         players = try databaseQueue.read { db in
           try Player.fetchAll(db)
+        }
+      } catch {
+        reportIssue(error)
+      }
+    }
+  }
+}
+
+struct PlayerDetailView: View {
+  let databaseQueue: DatabaseQueue
+  let player: Player
+  @State var team: Team?
+
+  var body: some View {
+    Form {
+      Section {
+        Text(player.name)
+        if player.isInjured {
+          Text("\(Image(systemName: "stethoscope")) Injured")
+            .foregroundStyle(.red)
+        } else {
+          Text("Not injured")
+        }
+      } header: {
+        Text("Details")
+      }
+
+      Section {
+        if let team {
+          Text(team.name)
+        } else {
+          Text("No team")
+            .italic()
+        }
+      } header: {
+        Text("Team")
+      }
+    }
+    .navigationTitle("Player")
+    .onAppear {
+      do {
+        guard let teamID = player.teamID
+        else { return }
+        team = try databaseQueue.read { db in
+          try Team.fetchOne(db, id: teamID)
         }
       } catch {
         reportIssue(error)
@@ -43,4 +102,15 @@ struct ContentView: View {
     }
   }
   return ContentView(databaseQueue: databaseQueue)
+}
+
+#Preview("Player detail") {
+  PlayerDetailView(
+    databaseQueue: try! .appDatabase(),
+    player: Player(
+      name: "Blob",
+      createdAt: Date(),
+      teamID: 3
+    )
+  )
 }
--- sqlite/SQLiteExplorations/SQLiteExplorations/Models.swift
+++ sqlite/SQLiteExplorations/SQLiteExplorations/Models.swift
@@ -1,13 +1,25 @@
 import Foundation
 import GRDB
 
-struct Player: MutablePersistableRecord, FetchableRecord, Codable {
+struct Player: MutablePersistableRecord, FetchableRecord, Codable, Identifiable {
   static let databaseTableName = "players"
 
   var id: Int64?
   var name = ""
   var createdAt: Date
   var isInjured = false
+  var teamID: Int64?
+
+  mutating func didInsert(_ inserted: InsertionSuccess) {
+    id = inserted.rowID
+  }
+}
+
+struct Team: MutablePersistableRecord, FetchableRecord, Codable, Identifiable {
+  static let databaseTableName = "teams"
+
+  var id: Int64?
+  var name = ""
 
   mutating func didInsert(_ inserted: InsertionSuccess) {
     id = inserted.rowID
@@ -20,6 +32,7 @@ extension DatabaseQueue {
       .path()
     print("open", databasePath)
     var config = Configuration()
+    config.foreignKeysEnabled = true
     config.prepareDatabase {
       $0.trace { print($0) }
     }
@@ -49,17 +62,31 @@ extension DatabaseQueue {
         table.add(column: "isInjured", .boolean).defaults(to: false)
       }
     }
+    migrator.registerMigration("Create 'teams' table") { db in
+      try db.create(table: Team.databaseTableName) { table in
+        table.autoIncrementedPrimaryKey("id")
+        table.column("name", .text).notNull()
+      }
+      try db.alter(table: Player.databaseTableName) { table in
+        table.add(column: "teamID", .integer)
+          .references("teams")
+      }
+    }
     #if DEBUG && targetEnvironment(simulator)
       migrator.registerMigration("Seed simulator data") { db in
-        _ = try Player(name: "Blob", createdAt: Date(), isInjured: false)
+        let lions = try Team(name: "Lions").inserted(db)
+        let tigers = try Team(name: "Tigers").inserted(db)
+        let bears = try Team(name: "Bears").inserted(db)
+        _ = try Player(name: "Blob", createdAt: Date(), isInjured: false, teamID: lions.id)
           .inserted(db)
-        _ = try Player(name: "Blob Jr", createdAt: Date(), isInjured: false)
+        _ = try Player(name: "Blob Jr", createdAt: Date(), isInjured: false, teamID: tigers.id)
           .inserted(db)
-        _ = try Player(name: "Blob Sr", createdAt: Date(), isInjured: true)
+        _ = try Player(name: "Blob Sr", createdAt: Date(), isInjured: true, teamID: bears.id)
           .inserted(db)
       }
     #endif
     try migrator.migrate(databaseQueue)
+
     return databaseQueue
   }
 }
