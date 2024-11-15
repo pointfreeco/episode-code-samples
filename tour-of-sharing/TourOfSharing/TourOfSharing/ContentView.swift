e7fd8341ec5dc4065d8ae4cfceb7efe13db54f96
Brandon Williams <mbrandonw@hey.com>
2024-11-15 14:20:48 -0500

    wip

--- tour-of-sharing/TourOfSharing/TourOfSharing/ContentView.swift
+++ tour-of-sharing/TourOfSharing/TourOfSharing/ContentView.swift
@@ -1,17 +1,51 @@
 import SwiftUI
 
-struct ContentView: View {
+struct ManyCountersView: View {
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+    Form {
+      Section {
+        CounterView()
+      }
+
+      Section {
+        CounterView()
+      }
+
+      Button(#"UserDefaults.set(0, "count")"#) {
+        UserDefaults.standard.set(
+          0,
+          forKey: "count"
+        )
+      }
     }
-    .padding()
   }
 }
 
-#Preview {
-  ContentView()
+@Observable
+class CounterModel {
+  @ObservationIgnored
+  @AppStorage("count") var count = 0
+}
+
+struct CounterView: View {
+  @State var model = CounterModel()
+
+  var body: some View {
+    Text("\(model.count)")
+      .font(.largeTitle)
+    Button("Decrement") {
+      model.count -= 1
+    }
+    Button("Increment") {
+      model.count += 1
+    }
+  }
+}
+
+#Preview("CounterView") {
+  CounterView()
+}
+
+#Preview("ManyCountersView") {
+  ManyCountersView()
 }
--- tour-of-sharing/TourOfSharing/TourOfSharing/TourOfSharingApp.swift
+++ tour-of-sharing/TourOfSharing/TourOfSharing/TourOfSharingApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct TourOfSharingApp: App {
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      ManyCountersView()
     }
   }
 }
