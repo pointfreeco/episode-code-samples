acad2ab844fb74b832c6a531cb24cd996edd1b3b
Brandon Williams <mbrandonw@hey.com>
2024-11-15 14:07:53 -0500

    wip

---
+++ tour-of-sharing/TourOfSharing/TourOfSharing/TourOfSharingApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct TourOfSharingApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ tour-of-sharing/TourOfSharing/TourOfSharingTests/TourOfSharingTests.swift
@@ -0,0 +1,17 @@
+//
+//  TourOfSharingTests.swift
+//  TourOfSharingTests
+//
+//  Created by Point-Free on 11/15/24.
+//
+
+import Testing
+@testable import TourOfSharing
+
+struct TourOfSharingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
