cfcf49b759b1923fa19a9a22f77f3d1e5a4aee74
Point-Free <pointfreeco@Mac.lan>
2024-11-15 13:02:48 -0800

    wip

--- tour-of-sharing/TourOfSharing/TourOfSharing/ContentView.swift
+++ tour-of-sharing/TourOfSharing/TourOfSharing/ContentView.swift
@@ -8,19 +8,37 @@ struct ManyCountersView: View {
     Form {
       Section {
         CounterView()
+      } header: {
+        Text("@Shared in an observable model")
       }
 
       Section {
-        CounterView()
+        AnotherCounterView()
+      } header: {
+        Text("@Shared in a view")
+      }
+
+      Section {
+        AppStorageCounterView()
+      } header: {
+        Text("@AppStorage")
+      }
+
+      Section {
+        CounterViewController.Representable()
+      } header: {
+        Text("@Shared in a view controller")
       }
 
       Button(#"UserDefaults.set(0, "count")"#) {
-        withAnimation {
-          $count.withLock { $0 = 0 }
-//          UserDefaults.standard.set(
-//            0,
-//            forKey: "count"
-//          )
+        UIView.animate(withDuration: 0.35) {
+          withAnimation {
+            $count.withLock { $0 = 0 }
+            //          UserDefaults.standard.set(
+            //            0,
+            //            forKey: "count"
+            //          )
+          }
         }
       }
     }
@@ -38,27 +56,53 @@ struct CounterView: View {
   @State var model = CounterModel()
 
   var body: some View {
-    Text("\(model.count)")
-      .font(.largeTitle)
-    Button("Decrement") {
-      //model.count -= 1
-      model.$count.withLock { $0 -= 1 }
+    HStack {
+      Text("\(model.count)")
+        .font(.largeTitle)
+      Button("Decrement") {
+        model.$count.withLock { $0 -= 1 }
+      }
+      Button("Increment") {
+        model.$count.withLock { $0 += 1 }
+      }
     }
-    Button("Increment") {
-      //model.count += 1
-      model.$count.withLock { $0 += 1 }
+    .buttonStyle(.borderless)
+  }
+}
+
+struct AnotherCounterView: View {
+  @Shared(.appStorage("count")) var count = 0
+
+  var body: some View {
+    HStack {
+      Text("\(count)")
+        .font(.largeTitle)
+      Button("Decrement") {
+        $count.withLock { $0 -= 1 }
+      }
+      Button("Increment") {
+        $count.withLock { $0 += 1 }
+      }
     }
-//    Button("Race!") {
-//      Task {
-//        await withTaskGroup(of: Void.self) { [sharedCount = model.$count] group in
-//          for _ in 1...1_000 {
-//            group.addTask {
-//              sharedCount.withLock { $0 += 1 }
-//            }
-//          }
-//        }
-//      }
-//    }
+    .buttonStyle(.borderless)
+  }
+}
+
+struct AppStorageCounterView: View {
+  @AppStorage("count") var count = 0
+
+  var body: some View {
+    HStack {
+      Text("\(count)")
+        .font(.largeTitle)
+      Button("Decrement") {
+        count -= 1
+      }
+      Button("Increment") {
+        count += 1
+      }
+    }
+    .buttonStyle(.borderless)
   }
 }
 
@@ -70,7 +114,75 @@ struct CounterView: View {
   ManyCountersView()
 }
 
+import Combine
 
+final class CounterViewController: UIViewController {
+  @Shared(.appStorage("count")) var count = 0
+  var cancellables: Set<AnyCancellable> = []
+
+  struct Representable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> CounterViewController {
+      CounterViewController()
+    }
+    func updateUIViewController(_ uiViewController: CounterViewController, context: Context) {
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let stackView = UIStackView()
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.distribution = .fillProportionally
+    stackView.spacing = 8
+    view.addSubview(stackView)
+
+    let countLabel = UILabel()
+    countLabel.textColor = .black
+    countLabel.font = .preferredFont(forTextStyle: .largeTitle)
+    stackView.addArrangedSubview(countLabel)
+
+    let decrementButton = UIButton(type: .system)
+    decrementButton.setTitle("Decrement", for: .normal)
+    decrementButton.titleLabel?.font = .preferredFont(forTextStyle: .body)
+    decrementButton.addAction(
+      UIAction { [$count] _ in
+        $count.withLock { $0 -= 1 }
+      },
+      for: .touchUpInside
+    )
+    stackView.addArrangedSubview(decrementButton)
+
+    let incrementButton = UIButton(type: .system)
+    incrementButton.setTitle("Increment", for: .normal)
+    incrementButton.titleLabel?.font = .preferredFont(forTextStyle: .body)
+    incrementButton.addAction(
+      UIAction { [$count] _ in
+        $count.withLock { $0 += 1 }
+      },
+      for: .touchUpInside
+    )
+    stackView.addArrangedSubview(incrementButton)
+
+    NSLayoutConstraint.activate([
+      stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      stackView.topAnchor.constraint(equalTo: view.topAnchor),
+      stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+      stackView.heightAnchor.constraint(equalToConstant: 300)
+    ])
+
+    $count.publisher
+      .sink {
+        countLabel.text = $0.description
+      }
+      .store(in: &cancellables)
+
+//    observe { [weak self] in
+//      guard let self else { return }
+//      countLabel.text = count.description
+//    }
+  }
+}
 
 struct AppStorageRaceCondition: View {
   @AppStorage("racey-count") var count = 0
