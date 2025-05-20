fbc9a436d06eb759615e2fce84a2feacb5097e1b
Stephen Celis <stephen@stephencelis.com>
2025-05-19 22:39:02 -0700

    wip

---
+++ 0324-modern-persistence-pt2/ModernPersistence/ModernPersistence/RemindersListsFeature.swift
@@ -0,0 +1,95 @@
+import SharingGRDB
+import SwiftUI
+
+@Observable
+class RemindersListsModel {
+  @ObservationIgnored
+  @Dependency(\.defaultDatabase) var database
+  @ObservationIgnored
+  @FetchAll(RemindersList.all) var remindersLists
+
+  func deleteButtonTapped(indexSet: IndexSet) {
+    withErrorReporting {
+      try database.write { db in
+        let ids = indexSet.map { remindersLists[$0].id }
+        try RemindersList
+          .where { $0.id.in(ids) }
+          .delete()
+          .execute(db)
+      }
+    }
+  }
+}
+
+struct RemindersListsView: View {
+  let model: RemindersListsModel
+
+  var body: some View {
+    List {
+      Section {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Top-level stats@*//*@END_MENU_TOKEN@*/
+      }
+
+      Section {
+        ForEach(model.remindersLists) { remindersList in
+          RemindersListRow(
+            incompleteRemindersCount: 0,
+            remindersList: remindersList
+          )
+        }
+        .onDelete { indexSet in
+          model.deleteButtonTapped(indexSet: indexSet)
+        }
+      } header: {
+        Text("My lists")
+          .font(.largeTitle)
+          .bold()
+          .foregroundStyle(.black)
+          .textCase(nil)
+      }
+
+      Section {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Tags@*//*@END_MENU_TOKEN@*/
+      } header: {
+        Text("Tags")
+          .font(.largeTitle)
+          .bold()
+          .foregroundStyle(.black)
+          .textCase(nil)
+      }
+    }
+    .searchable(text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Search text@*/.constant("")/*@END_MENU_TOKEN@*/)
+    .toolbar {
+      ToolbarItem(placement: .bottomBar) {
+        HStack {
+          Button {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=New reminder action@*//*@END_MENU_TOKEN@*/
+          } label: {
+            HStack {
+              Image(systemName: "plus.circle.fill")
+              Text("New Reminder")
+            }
+            .bold()
+            .font(.title3)
+          }
+          Spacer()
+          Button {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Add list action@*//*@END_MENU_TOKEN@*/
+          } label: {
+            Text("Add List")
+              .font(.title3)
+          }
+        }
+      }
+    }
+  }
+}
+
+#Preview {
+  let _ = prepareDependencies {
+    $0.defaultDatabase = try! appDatabase()
+  }
+  NavigationStack {
+    RemindersListsView(model: RemindersListsModel())
+  }
+}
---
+++ 0324-modern-persistence-pt2/README.md
@@ -0,0 +1,5 @@
+## [Point-Free](https://www.pointfree.co)
+
+> #### This directory contains code from Point-Free Episode: [Modern Persistence: Reminders Lists, Part 1](https://www.pointfree.co/episodes/ep324-modern-persistence-reminders-lists-part-1)
+>
+> We tackle the first screen in our Reminders app rewrite: the reminders lists view. We will take the `@FetchAll` property wrapper for a spin, which is like SwiftData’s `@Query` macro, but unlike `@Query` it can be used from both the view and observable models. And we will even get some end-to-end, snapshot test coverage of our feature in place.
