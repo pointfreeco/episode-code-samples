bfc9c1ad1168e305a9230c364d451fea60fcbc67
Brandon Williams <mbrandonw@hey.com>
2025-12-04 15:48:00 -0600

    wip

--- sqlite-data-tour/Scorekeeper/Scorekeeper/GameFeature.swift
+++ sqlite-data-tour/Scorekeeper/Scorekeeper/GameFeature.swift
@@ -8,6 +8,7 @@ import SwiftUI
   var isPlayerPhotoPickerPresented = false
   var playerPhotoPickerPresented: Player?
   var newPlayerName = ""
+  var playerPhotoConfirmationDialog: Player?
   var photosPickerItem: PhotosPickerItem? {
     didSet {
       updatePlayerImageTask?.cancel()
@@ -86,9 +87,30 @@ import SwiftUI
     await reloadData()
   }
 
-  func photoButtonTapped(for player: Player) {
+  func photoButtonTapped(row: Row) {
+    if row.imageData == nil {
+      isPlayerPhotoPickerPresented = true
+      playerPhotoPickerPresented = row.player
+    } else {
+      playerPhotoConfirmationDialog = row.player
+    }
+  }
+
+  func selectNewPhotoButtonTapped() {
+    guard let player = playerPhotoConfirmationDialog
+    else { return }
     isPlayerPhotoPickerPresented = true
     playerPhotoPickerPresented = player
+  }
+
+  func removePhotoButtonTapped() {
+    guard let player = playerPhotoConfirmationDialog
+    else { return }
+    withErrorReporting {
+      try database.write { db in
+        try PlayerAsset.find(player.id).delete().execute(db)
+      }
+    }
   }
 
   private func reloadData() async {
@@ -150,7 +172,7 @@ struct GameView: View {
           ForEach(model.rows, id: \.player.id) { row in
             HStack {
               Button {
-                model.photoButtonTapped(for: row.player)
+                model.photoButtonTapped(row: row)
               } label: {
                 if
                   let imageData = row.imageData,
@@ -166,6 +188,17 @@ struct GameView: View {
               .frame(width: 44, height: 44)
               .clipShape(Circle())
               .transaction { $0.animation = nil }
+              .confirmationDialog(
+                "Player photo",
+                isPresented: $model.playerPhotoConfirmationDialog[isPresenting: row.player.id]
+              ) {
+                Button("Select new photo") {
+                  model.selectNewPhotoButtonTapped()
+                }
+                Button("Remove photo", role: .destructive) {
+                  model.removePhotoButtonTapped()
+                }
+              }
 
               Text(row.player.name)
               Spacer()
@@ -226,6 +259,17 @@ struct GameView: View {
       isPresented: $model.isPlayerPhotoPickerPresented,
       selection: $model.photosPickerItem
     )
+  }
+}
+
+extension Optional {
+  subscript(isPresenting id: Wrapped.ID) -> Bool where Wrapped: Identifiable {
+    get {
+      self?.id == id
+    }
+    set {
+      if !newValue { self = nil }
+    }
   }
 }
 
