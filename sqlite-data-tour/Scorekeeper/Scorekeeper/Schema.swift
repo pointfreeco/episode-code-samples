c997675442506348f1941074e902d29806824ef7
Point-Free <pointfreeco@Mac.lan>
2025-12-16 13:51:30 -0800

    wip

--- sqlite-data-tour/Scorekeeper/Scorekeeper/Schema.swift
+++ sqlite-data-tour/Scorekeeper/Scorekeeper/Schema.swift
@@ -85,6 +85,10 @@ func appDatabase() throws -> any DatabaseWriter {
 extension DependencyValues {
   mutating func bootstrapDatabase() throws {
     defaultDatabase = try appDatabase()
+    defaultSyncEngine = try SyncEngine(
+      for: defaultDatabase,
+      tables: Game.self, Player.self, PlayerAsset.self,
+    )
   }
 }
 
