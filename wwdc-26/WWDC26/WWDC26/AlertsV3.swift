dbdae29dc47d9a21e35305aff110baa8f4ad86af
pointfreeco <pointfreeco@Brandons-MacBook-Pro.local>
2026-06-24 14:47:40 -0500

    wip

--- wwdc-26/WWDC26/WWDC26/AlertsV3.swift
+++ wwdc-26/WWDC26/WWDC26/AlertsV3.swift
@@ -2,14 +2,13 @@ import SwiftUI
 import SwiftUINavigation
 
 struct AlertsV3View: View {
-  //  @State private var deleteAlertIsPresented = false
-  //  @State private var deleteConfirmation = ""
   @State private var deleteConfirmation: String?
 
   @State private var alertAction: Action?
+  @CaseBindable
   enum Action: Identifiable {
-    var id: Self { self }
-    case delete
+    var id: some Hashable { Self.allCasePaths[self] }
+    case delete(confirmation: String)
     case archive
   }
 
@@ -17,11 +16,7 @@ struct AlertsV3View: View {
     Form {
       Section {
         Button("Delete") {
-          alertAction = .delete
-          _ = Task {
-            try await Task.sleep(for: .seconds(1))
-            alertAction = .archive
-          }
+          alertAction = .delete(confirmation: "")
         }
         Button("Archive") {
           alertAction = .archive
@@ -44,29 +39,18 @@ struct AlertsV3View: View {
     } message: { _ in
       Text("Enter name to confirm")
     }
-    //    .alert("Delete", isPresented: $deleteAlertIsPresented) {
-    //      Button("Confirm", role: .destructive) {
-    //        print(deleteConfirmation == "pointfreeco" ? "✅" : "🛑")
-    //        deleteConfirmation = ""
-    //      }
-    //      Button("Cancel", role: .cancel) {
-    //        deleteConfirmation = ""
-    //      }
-    //      TextField("pointfreeco", text: $deleteConfirmation)
-    //        .textInputAutocapitalization(.never)
-    //    } message: {
-    //      Text("Are you sure?")
-    //    }
     .alert(item: $alertAction) {
       switch $0 {
       case .delete: Text("Delete")
       case .archive: Text("Archive")
       }
-    } actions: { action in
-      switch action {
-      case .delete:
-        Button("Confirm", role: .destructive) {}
-        TextField("pointfreeco", text: .constant(""))
+    } actions: { $action in
+      switch $action.cases {
+      case .delete(let $confirmation):
+        Button("Confirm", role: .destructive) {
+          print($confirmation.wrappedValue == "pointfreeco" ? "✅" : "🛑")
+        }
+        TextField("pointfreeco", text: $confirmation)
           .textInputAutocapitalization(.never)
       case .archive:
         Button("Confirm") {}
@@ -80,33 +64,6 @@ struct AlertsV3View: View {
         Text("Do you want to archive?")
       }
     }
-    //    .alert(
-    //      alertAction.map {
-    //        switch $0 {
-    //        case .delete: "Delete"
-    //        case .archive: "Archive"
-    //        }
-    //      }
-    //        ?? "",
-    //      item: $alertAction
-    //    ) { action in
-    //      switch action {
-    //      case .delete:
-    //        Button("Confirm", role: .destructive) {}
-    //        TextField("pointfreeco", text: .constant(""))
-    //          .textInputAutocapitalization(.never)
-    //      case .archive:
-    //        Button("Confirm") {}
-    //        Button(role: .cancel) {}
-    //      }
-    //    } message: { action in
-    //      switch action {
-    //      case .delete:
-    //        Text("Confirm name to delete")
-    //      case .archive:
-    //        Text("Do you want to archive?")
-    //      }
-    //    }
   }
 }
 
